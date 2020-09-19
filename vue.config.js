734294fcf659ea37d6bb142a785a48728f0fdebb
David Chen <juandavidchen@gmail.com>
2020-09-18 19:45:35 -0700

    service worker registration fix #2

--- vue.config.js
+++ vue.config.js
@@ -6,12 +6,12 @@ module.exports = {
         appleMobileWebAppCapable: "yes",
         appleMobileWebAppStatusBarStyle: "black",
 
-        // configure the workbox plugin
-        workboxPluginMode: "InjectManifest",
-        workboxOptions: {
-            // swSrc is required in InjectManifest mode.
-            swSrc: "./src/registerServiceWorker.js",
-            // ...other Workbox options...
-        },
+        // // configure the workbox plugin
+        // workboxPluginMode: "InjectManifest",
+        // workboxOptions: {
+        //     // swSrc is required in InjectManifest mode.
+        //     swSrc: "./src/registerServiceWorker.js",
+        //     // ...other Workbox options...
+        // },
     },
 };
