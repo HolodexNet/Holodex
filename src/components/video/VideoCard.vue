3257cdd7b07205c11b2ea23406eae4babd1eb6ee
David Chen <juandavidchen@gmail.com>
2021-01-14 01:27:20 -0800

    fix null check mentions

--- src/components/video/VideoCard.vue
+++ src/components/video/VideoCard.vue
@@ -65,7 +65,7 @@
                         <router-link
                             :to="`/channel/${video.channel.id}`"
                             class="no-decoration channel-name"
-                            :class="`name-${video.channel.type}`"
+                            :class="{ 'name-vtuber': video.type === 'stream' || channel.type === 'vtuber' }"
                         >
                             {{ channelName }}
                         </router-link>
