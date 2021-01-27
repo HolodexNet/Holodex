c4a9c1da07d2c45bb1eeacac813803d0d811a970
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2021-01-26 22:22:56 -0800

    song search fetches from itunes

--- src/components/media/SongSearch.vue
+++ src/components/media/SongSearch.vue
@@ -4,78 +4,68 @@
     <v-autocomplete
         class="ma-auto song-lookup"
         solo
-        flat
-        multiple
-        deletable-chips
-        chips
+        filled
         disable-lookup
-        clearable
         hide-no-data
-        hide-selected
         auto-select-first
         dense
-        :rules="[validate]"
+        clearable
         :autofocus="autofocus"
         :small-chips="dense"
         v-model="query"
         :loading="isLoading"
         :items="results"
+        :item-value="(x) => x.trackId"
         :search-input.sync="search"
         @input="onInput"
-        :append-icon="''"
         label="Lookup on iTunes"
         :filter="(a, b) => true"
         return-object
         @keydown.enter="onEnterKeyDown"
         hide-details
     >
-        <template v-slot:selection="selection">
-            <v-card
-                :color="$vuetify.theme.dark ? 'grey darken-3' : 'light-blue accent-4'"
-                :label="selection.item.type !== 'channel'"
-                class="pa-0 selected-card"
-                :dark="$vuetify.theme.dark"
-            >
-                <v-list-item class="ma-n1 py-0 pl-3 pr-1">
-                    {{ dropdownItem }}
-
-                    <!-- <div class="selected-card-type px-1 py-0 ma-0 rounded text--disabled caption">
-                        <v-icon x-small v-if="selection.item.type === 'channel'">{{ icons.mdiYoutube }}</v-icon>
-                        <v-icon x-small v-if="selection.item.type === 'topic'">{{ icons.mdiAnimationPlay }}</v-icon>
-                        <v-icon x-small v-if="selection.item.type === 'org'">{{ mdiAccountMultiple }}</v-icon>
-                        <v-icon x-small v-if="selection.item.type === 'title & desc'">{{ mdiTextSearch }}</v-icon>
-                        <v-icon x-small v-if="selection.item.type === 'comments'">{{ mdiCommentSearch }}</v-icon>
-                        {{ i18nItem(selection.item.type) }}
-                    </div>
-
-                    <v-list-item-content class="py-1 pt-4">
-                        <v-list-item-subtitle
-                            class="text--primary search-item"
-                            v-text="selection.item.text"
-                        ></v-list-item-subtitle>
-                    </v-list-item-content>
-
-                    <v-list-item-action>
-                        <v-icon small color="red accent-2" @click="deleteChip(selection.item)">{{
-                            icons.mdiClose
-                        }}</v-icon>
-                    </v-list-item-action> -->
-                </v-list-item>
-            </v-card>
-        </template>
-        <template v-slot:item="dropdownItem">
-            <div class="ma-n1 py-0 pl-3 pr-1">
+        <template v-slot:selection="x">
+            <div class="ma-n1 py-0 pl-3 pr-1 d-flex">
                 <!-- @click="addItem(dropdownItem.item) -->
+                <v-list-item-avatar tile>
+                    <v-img :src="x.item.artworkUrl100"></v-img>
+                </v-list-item-avatar>
+
                 <v-list-item-content class="py-1 pt-1">
                     <v-list-item-subtitle class="text--primary">
-                        {{ dropdownItem }}
+                        🎵 {{ x.item.trackName }} [{{ Math.floor(x.item.trackTimeMillis / 1000 / 60) }}:{{
+                            Math.round(x.item.trackTimeMillis / 1000) % 60
+                        }}]
+                    </v-list-item-subtitle>
+                    <v-list-item-subtitle class="text--caption">
+                        🎤 {{ x.item.artistName }} / {{ x.item.collectionName }} / {{ x.item.releaseDate.slice(0, 7) }}
                     </v-list-item-subtitle>
                 </v-list-item-content>
             </div>
         </template>
-        <template v-slot:append-outer>
-            <v-icon key="searchbtn" large color="info" @click="commitSearch" v-text="icons.mdiMagnify"></v-icon>
+
+        <template v-slot:item="x">
+            <div class="ma-n1 py-0 pl-3 pr-1 d-flex">
+                <!-- @click="addItem(dropdownItem.item) -->
+                <v-list-item-avatar tile>
+                    <v-img :src="x.item.artworkUrl100"></v-img>
+                </v-list-item-avatar>
+
+                <v-list-item-content class="py-1 pt-1">
+                    <v-list-item-subtitle class="text--primary">
+                        🎵 {{ x.item.trackName }} [{{ Math.floor(x.item.trackTimeMillis / 1000 / 60) }}:{{
+                            Math.round(x.item.trackTimeMillis / 1000) % 60
+                        }}]
+                    </v-list-item-subtitle>
+                    <v-list-item-subtitle class="text--caption">
+                        🎤 {{ x.item.artistName }} / {{ x.item.collectionName }} / {{ x.item.releaseDate.slice(0, 7) }}
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </div>
         </template>
+        <!-- <template v-slot:append-outer>
+            <v-icon key="searchbtn" large color="info" @click="commitSearch" v-text="icons.mdiMagnify"></v-icon>
+        </template> -->
     </v-autocomplete>
 </template>
 
@@ -92,6 +82,7 @@ import * as icons from "@/utils/icons";
 import { debounce } from "@/utils/functions";
 import { json2csvAsync, csv2jsonAsync } from "json-2-csv";
 import jsonp from "jsonp-es6";
+import { formatDuration } from "@/utils/time";
 
 export default {
     name: "SongSearch",
@@ -147,13 +138,28 @@ export default {
             const entropy = encodeURIComponent(val).length;
             if (entropy <= 2) return;
             // const formatted = val.replace("#", "").toLowerCase();
-            jsonp();
-        }),
+            // jsonp();
+            this.getAutocomplete(val);
+        }, 500),
     },
     methods: {
+        formatDuration,
         async getAutocomplete(query) {
             this.isLoading = true;
             const res = await this.searchAutocomplete(query);
+            if (res && res.results) {
+                this.fromApi = res.results.map(
+                    ({ trackId, collectionName, releaseDate, artistName, trackName, artworkUrl100, trackViewUrl }) => ({
+                        trackId,
+                        collectionName,
+                        releaseDate,
+                        artistName,
+                        trackName,
+                        artworkUrl100,
+                        trackViewUrl,
+                    }),
+                );
+            }
             console.log(res);
             this.isLoading = false;
             return res;
@@ -163,12 +169,9 @@ export default {
                 term: query,
                 entity: "musicTrack",
                 country: "JP",
-                limit: 30,
+                limit: 10,
                 lang: "ja_jp",
             });
-        },
-        deleteChip(item) {
-            this.query.splice(this.query.map((q) => q.value).indexOf(item.value), 1);
         },
         async commitSearch() {
             if (!this.query || this.query.length === 0) return;
@@ -190,16 +193,6 @@ export default {
         onInput() {
             this.search = null;
             this.fromApi = [];
-        },
-        validate(currentQuery) {
-            // current limitations:
-            // if more than 1 comment search, fail
-            // if text search AND comment search, fail
-            const countcomments = currentQuery.filter((x) => x.type === "comments").length;
-            if (countcomments > 1) return "Cannot search using multiple comment conditions.";
-            if (countcomments === 1 && currentQuery.filter((x) => x.type === "title & desc").length > 0)
-                return "Cannot search using comment + title & desc filters at the same time.";
-            return true;
         },
         onEnterKeyDown() {
             if (this.search === null || this.search.length === 0) {
