1102ebe7505e853358808bb83191c64dfaae3f18
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2021-02-22 01:52:21 -0800

    rip out storybook

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,22 @@
 module.exports = {
     chainWebpack: (config) => {
-        if (process.env.STORYBOOK && process.env.STORYBOOK.trim() === "true") {
-            console.info("info => Updating webpack using chain-webpack");
-            // eslint-disable-next-line no-param-reassign
-            config.module
-                .rule("addon-storysource")
-                .enforce()
-                .pre()
-                .test(/\.stories\.jsx?$/)
-                .use("@storybook/addon-storysource/loader")
-                .loader("@storybook/addon-storysource/loader")
-                .options({
-                    semi: false,
-                    printWidth: 120,
-                    singleQuote: true,
-                })
-                .end();
-        }
+        // if (process.env.STORYBOOK && process.env.STORYBOOK.trim() === "true") {
+        //     console.info("info => Updating webpack using chain-webpack");
+        //     // eslint-disable-next-line no-param-reassign
+        //     config.module
+        //         .rule("addon-storysource")
+        //         .enforce()
+        //         .pre()
+        //         .test(/\.stories\.jsx?$/)
+        //         .use("@storybook/addon-storysource/loader")
+        //         .loader("@storybook/addon-storysource/loader")
+        //         .options({
+        //             semi: false,
+        //             printWidth: 120,
+        //             singleQuote: true,
+        //         })
+        //         .end();
+        // }
         return config;
     },
     css: {
