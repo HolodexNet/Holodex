7de2b6b690638280cc173f06ec1c34fa5498e16b
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2021-03-21 19:13:37 -0700

    library export and guidance.

--- src/views/Library.vue
+++ src/views/Library.vue
@@ -6,9 +6,41 @@
                     {{ $t("views.library.savedVideosTitle") }}
                 </div>
                 <div>
-                    <v-btn class="mr-1 mb-1" color="green" @click="exportSelected">
-                        {{ $t("views.library.createYtPlaylistButton", [selected.length]) }}
+                    <v-btn class="mr-1 mb-1" @click="showReset ? reset() : selectAll()" color="blue-grey">
+                        {{ showReset ? $t("views.library.selectionReset") : $t("views.library.selectionSelectAll") }}
                     </v-btn>
+                    <v-btn class="mr-1 mb-1" @click="select(50)" v-if="!showReset" color="blue-grey">
+                        {{ $t("views.library.selectionSelect50") }}
+                    </v-btn>
+
+                    <v-menu>
+                        <template v-slot:activator="{ on }">
+                            <v-btn v-on="on" class="mr-1 mb-1" color="green darken-2">
+                                {{ $t("views.library.exportSelected", [selected.length]) }}
+                            </v-btn>
+                        </template>
+                        <v-list dense>
+                            <v-list-item-group>
+                                <v-list-item @click.stop="instructionsDialog = true">
+                                    <v-list-item-icon>
+                                        <v-icon>{{ icons.mdiYoutube }}</v-icon>
+                                    </v-list-item-icon>
+                                    <v-list-item-content>{{
+                                        $t("views.library.exportYtPlaylist")
+                                    }}</v-list-item-content>
+                                </v-list-item>
+                                <v-list-item @click.stop="downloadAsCSV">
+                                    <v-list-item-icon>
+                                        <v-icon>{{ mdiFileTable }}</v-icon>
+                                    </v-list-item-icon>
+                                    <v-list-item-content>{{ $t("views.library.exportCsv") }}</v-list-item-content>
+                                </v-list-item>
+                            </v-list-item-group>
+                        </v-list>
+                    </v-menu>
+                    <!-- <v-btn class="mr-1 mb-1" color="green" @click="exportSelected">
+                        {{ $t("views.library.createYtPlaylistButton", [selected.length]) }}
+                    </v-btn> -->
                     <v-dialog v-model="deleteDialog" max-width="290">
                         <template v-slot:activator="{ on, attrs }">
                             <v-btn color="red" class="mr-1 mb-1" v-bind="attrs" v-on="on">
@@ -38,9 +70,6 @@
                             </v-card-actions>
                         </v-card>
                     </v-dialog>
-                    <v-btn class="mr-1 mb-1" @click="showReset ? reset() : selectAll()" color="blue-grey">
-                        {{ showReset ? $t("views.library.selectionReset") : $t("views.library.selectionSelectAll") }}
-                    </v-btn>
                 </div>
             </v-col>
         </v-row>
@@ -80,11 +109,41 @@
                 </v-list-item>
             </v-list>
         </v-card>
+        <v-dialog v-model="instructionsDialog" :width="$store.state.isMobile ? '90%' : '60vw'">
+            <v-card>
+                <v-card-title>{{ $t("views.library.exportYTHeading") }}</v-card-title>
+                <v-card-text>
+                    <v-row>
+                        <v-col cols="">
+                            <p>
+                                {{ $t("views.library.exportYTExplanation") }}
+                            </p>
+                            <br />
+
+                            <br />
+                            <p>
+                                {{ $t("views.library.exportYTInstructions") }}
+                            </p>
+                            <v-btn class="mt-2 mx-2" color="green" @click="exportSelected">
+                                {{ $t("views.library.createYtPlaylistButton", [selected.length]) }}
+                            </v-btn>
+                            <v-btn class="mt-2 mx-2">{{ $t("views.library.deleteConfirmationCancel") }}</v-btn>
+                        </v-col>
+                        <v-col cols="12" md="auto">
+                            <img src="/img/playlist-instruction.jpg" />
+                        </v-col>
+                    </v-row>
+                </v-card-text>
+                <v-card-actions> </v-card-actions>
+            </v-card>
+        </v-dialog>
     </v-container>
 </template>
 
 <script>
 import VideoCardList from "@/components/video/VideoCardList";
+import { mdiFileTable } from "@mdi/js";
+import { json2csvAsync } from "json-2-csv";
 
 export default {
     name: "Library",
@@ -101,10 +160,12 @@ export default {
     },
     data() {
         return {
+            mdiFileTable,
             selected: [],
             deleteDialog: false,
             recoveredVideos: [],
             recoveredUrl: "",
+            instructionsDialog: false,
         };
     },
     created() {
@@ -143,6 +204,9 @@ export default {
         selectAll() {
             this.selected = this.savedVideosList.map((v) => v.id);
         },
+        select(n) {
+            this.selected = this.savedVideosList.slice(0, n).map((v) => v.id);
+        },
         reset() {
             this.selected = [];
         },
@@ -162,6 +226,17 @@ export default {
         clearOldStorage() {
             localStorage.removeItem("holodex");
             this.$router.go(0);
+        },
+        async downloadAsCSV() {
+            const selectedSet = new Set(this.selected);
+            const csvString = await json2csvAsync(this.savedVideosList.filter((v) => selectedSet.has(v.id)));
+            const a = document.createElement("a");
+            a.href = `data:attachment/csv,${encodeURIComponent(csvString)}`;
+            a.target = "_blank";
+            a.download = "holodexPlaylist.csv";
+
+            document.body.appendChild(a);
+            a.click();
         },
     },
 };
