c5fe1677d34e87e78e466aac925c6b4a51cfe43a
D3fau4 <19638408+D3fau4@users.noreply.github.com>
2021-04-13 18:58:33 +0200

    Even more text peko
    Co-Authored-By: Alejandro Hernández Pérez <52318740+Darkc0m@users.noreply.github.com>

--- src/views/channel_views/ChannelAbout.vue
+++ src/views/channel_views/ChannelAbout.vue
@@ -2,9 +2,9 @@
     <v-container>
         <v-row>
             <v-col sm="12" class="col-12 col-md-3">
-                <strong>Stats</strong>
+                <strong>{{ $t("component.channelInfo.stats") }}</strong>
                 <v-divider class="my-3" />
-                {{ channel.video_count }} {{ $t("component.channelInfo.video") }}
+                {{ channel.video_count }} {{ $t("component.channelInfo.videos") }}
                 <v-divider class="my-3" />
                 {{ channel.clip_count }} {{ $t("component.channelInfo.clips") }}
                 <v-divider class="my-3" />
