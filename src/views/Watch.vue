8091c7f1b81f984fff3374d0967cba884a97c423
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2021-04-16 03:01:40 -0700

    oops

--- src/views/Watch.vue
+++ src/views/Watch.vue
@@ -14,7 +14,6 @@
                     <WatchFrame :video="video">
                         <template v-slot:youtube>
                             <youtube
-                                :key="video.id + 'frame'"
                                 v-if="video.id"
                                 :video-id="video.id"
                                 @ready="ready"
@@ -134,7 +133,6 @@
                         <youtube
                             v-if="video.id"
                             :video-id="video.id"
-                            :key="video.id + 'frame'"
                             @ready="ready"
                             :playerVars="{
                                 ...(timeOffset && { start: timeOffset }),
