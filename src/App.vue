79ceb97c4332ddd232d1d4a2f8263cfb3c5d661f
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2021-04-25 19:51:44 -0700

    intercept and log axios errors

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,7 @@
 import MainNav from "@/components/nav/MainNav.vue";
 import pulltorefresh from "vue-awesome-pulltorefresh";
 import { dayjsLangs } from "./plugins/vuetify";
+import { axiosInstance } from "./utils/backend-api";
 
 export default {
     name: "App",
@@ -74,6 +75,39 @@ export default {
         };
     },
     created() {
+        axiosInstance.interceptors.response.use(undefined, (error) => {
+            // Any status codes that falls outside the range of 2xx cause this function to trigger
+            // Do something with response error
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                this.$gtag.exception({
+                    description: `${error.response.config.method} ${error.response.config.url}->${error.response.status}`,
+                    fatal: true,
+                });
+                console.error(error.response.data);
+                console.error(error.response.status);
+                console.error(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                this.$gtag.exception({
+                    description: "No Response Received",
+                    fatal: true,
+                });
+                console.error(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                this.$gtag.exception({
+                    description: `Generic: ${error.message}`,
+                    fatal: true,
+                });
+                console.error("Error", error.message);
+            }
+            return Promise.reject(error);
+        });
+
         // set theme
         this.$vuetify.theme.dark = this.darkMode;
         // set lang
