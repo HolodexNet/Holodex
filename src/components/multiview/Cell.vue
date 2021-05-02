4044d75bd2fc3818249d6d5b6af8c021f40b19d7
David Chen <3145205+RiceCakess@users.noreply.github.com>
2021-05-02 03:30:10 -0700

    Fix cells being unmuted when adding new cell

--- src/components/multiview/Cell.vue
+++ src/components/multiview/Cell.vue
@@ -205,8 +205,6 @@ export default {
                 })
             ) {
                 this.muted = true;
-            } else {
-                this.muted = false;
             }
         },
         pausedMode(newMode) {
