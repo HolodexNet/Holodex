8fa3ff35d037f02a48474593484283ea3154e9ac
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2021-07-12 10:31:30 -0700

    try resetting

--- src/store/index.js
+++ src/store/index.js
@@ -181,16 +181,19 @@ const migrations = [
     {
         version: 7,
         up: (state) => {
-            return {
-                ...state,
-                currentOrg: { name: "Hololive", short: "Holo" },
-                orgFavorites: [
-                    { name: "All Vtubers", short: "Vtuber" },
-                    { name: "Hololive", short: "Holo" },
-                    { name: "Nijisanji", short: "Niji" },
-                    { name: "Independents", short: "Indie" },
-                ],
-            };
+            if (state.currentOrg.name.includes('short"')) {
+                return {
+                    ...state,
+                    currentOrg: { name: "Hololive", short: "Holo" },
+                    orgFavorites: [
+                        { name: "All Vtubers", short: "Vtuber" },
+                        { name: "Hololive", short: "Holo" },
+                        { name: "Nijisanji", short: "Niji" },
+                        { name: "Independents", short: "Indie" },
+                    ],
+                };
+            }
+            return state;
         },
     },
 ];
