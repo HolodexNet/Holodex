f9764168221570bdf86f22c393286c88d9e66497
jprochazk <honza.spacir@gmail.com>
2021-08-07 15:57:03 +0200

    fix env vars

--- vite.config.js
+++ vite.config.js
@@ -5,30 +5,23 @@ import yaml from "@rollup/plugin-yaml";
 import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
-export default ({ mode }) => {
-    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-    const API_BASE_URL = process.env.API_BASE_URL || "https://staging.holodex.net";
-    const REWRITE_API_PATH = !!process.env.REWRITE_API_PATH;
-
-    return defineConfig({
-        plugins: [
-            yaml(),
-            createVuePlugin(),
-            ViteComponents({
-                customComponentResolvers: [VuetifyResolver()]
-            })
-        ],
-        resolve: {
-            alias: [
-                {
-                    find: "@",
-                    replacement: path.resolve(__dirname, "src")
-                }
-            ]
-        },
-        server: {
-            port: 8080
-        }
-    });
-}
+export default defineConfig({
+    plugins: [
+        yaml(),
+        createVuePlugin(),
+        ViteComponents({
+            customComponentResolvers: [VuetifyResolver()]
+        })
+    ],
+    resolve: {
+        alias: [
+            {
+                find: "@",
+                replacement: path.resolve(__dirname, "src")
+            }
+        ]
+    },
+    server: {
+        port: 8080
+    }
+});
