2bf6c5941f016a22ab065d46a37d3682777eb89f
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2021-08-08 00:38:17 -0700

    public/index.html instead =/

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
 import { createVuePlugin } from "vite-plugin-vue2";
 import ViteComponents, { VuetifyResolver } from "vite-plugin-components";
+import { visualizer } from 'rollup-plugin-visualizer';
 import yaml from "@rollup/plugin-yaml";
 import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
+const { resolve } = require('path')
+
 
 export default defineConfig({
     plugins: [
@@ -11,7 +14,66 @@ export default defineConfig({
         createVuePlugin(),
         ViteComponents({
             customComponentResolvers: [VuetifyResolver()]
-        })
+        }),
+        VitePWA({
+            manifest: {
+              // content of manifest
+                display: "standalone",
+                theme_color:"#42a5f5",
+                name: "Holodex",
+                background_color: "#42a5f5",
+                start_url: "/?utm_source=homescreen",
+                scope: "/",
+            },
+            workbox: {
+                swDest: "./dist/service-worker.js",
+                runtimeCaching: [
+                    {
+                        urlPattern: new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
+                        handler: "CacheFirst",
+                        options: {
+                            cacheName: "google-fonts",
+                            expiration: {
+                                maxEntries: 30,
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200],
+                            },
+                        },
+                    },
+                    {
+                        urlPattern: new RegExp("https://yt3.ggpht.com/(a/|ytc/)(.*)=s(40|88)-c-k-c0x00ffffff-no-rj-mo(.*)"),
+                        handler: "CacheFirst",
+                        options: {
+                            cacheName: "channel-photo",
+                            expiration: {
+                                maxAgeSeconds: 86400,
+                                purgeOnQuotaError: true,
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200],
+                            },
+                        },
+                    },
+                    {
+                        urlPattern: new RegExp("https://www.youtube.com/player_api"),
+                        handler: "CacheFirst",
+                        options: {
+                            cacheName: "youtube-player",
+                            expiration: {
+                                maxAgeSeconds: 10800,
+                                maxEntries: 1,
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200],
+                            },
+                        },
+                    },
+                ],
+              // workbox options for generateSW
+            }
+          }),
+        visualizer({gzipSize: true,}),
     ],
     resolve: {
         alias: [
@@ -20,6 +82,20 @@ export default defineConfig({
                 replacement: path.resolve(__dirname, "src")
             }
         ]
+    },
+    build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'public/index.html'),
+            seo: resolve(__dirname, 'public/seo.html')
+          },
+          output: {
+              dir: 'dist',
+          }
+        }
+    },
+    output: {
+      dir: 'dist'
     },
     server: {
         port: 8080
