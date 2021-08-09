d69cbc8a59bc2052087c73f684f27d5c7b8ddf36
David Chen <3145205+RiceCakess@users.noreply.github.com>
2021-08-08 20:41:10 -0700

    Fix missing icon

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
             customComponentResolvers: [VuetifyResolver()],
         }),
         VitePWA({
+            includeAssets: ["favicon.ico", "robots.txt", "img/icons/safari-pinned-tab.svg"],
             manifest: {
                 // content of manifest
                 display: "standalone",
@@ -37,13 +38,7 @@ export default defineConfig({
                         type: "image/png",
                     },
                     {
-                        src: "img/icons/android-chrome-maskable-192x192.png",
-                        sizes: "192x192",
-                        type: "image/png",
-                        purpose: "maskable",
-                    },
-                    {
-                        src: "img/icons/android-chrome-maskable-512x512.png",
+                        src: "img/icons/android-chrome-512x512.png",
                         sizes: "512x512",
                         type: "image/png",
                         purpose: "maskable",
