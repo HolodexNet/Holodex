abad999763ce6c1199bc8e9cead9320add8e1492
David Chen <3145205+RiceCakess@users.noreply.github.com>
2021-08-25 22:29:49 -0700

    Sigh smh

--- vite.config.js
+++ vite.config.js
@@ -23,8 +23,6 @@ export default ({ mode }) => {
                 customComponentResolvers: [VuetifyResolver()],
             }),
             VitePWA({
-                srcDir: "src",
-                filename: "sw.ts",
                 includeAssets: ["favicon.ico", "robots.txt", "img/icons/safari-pinned-tab.svg"],
                 manifest: {
                     // content of manifest
@@ -56,7 +54,7 @@ export default ({ mode }) => {
                 workbox: {
                     // NOTE: `vite-plugin-pwa` expects the service worker to be called `sw.js` for some reason.
                     // there is no way to change this.
-                    // swDest: "./dist/sw.js",
+                    swDest: "./dist/sw.js",
                     navigateFallbackDenylist: [/^\/api/],
                     runtimeCaching: [
                         {
