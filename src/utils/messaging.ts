00d49d51adc8fa0219e7bf6d5960391108c1465f
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2021-09-04 16:53:18 -0700

    remove chrome error msg

--- src/utils/messaging.ts
+++ src/utils/messaging.ts
@@ -1,13 +1,13 @@
 import { companionExtensionId, MESSAGE_TYPES } from "@/utils/consts";
 
 export const sendTokenToExtension = (token: string | null): void => {
-    if (chrome && chrome.runtime && chrome.runtime.sendMessage) {
+    if (window.chrome && chrome.runtime && chrome.runtime.sendMessage) {
         chrome.runtime.sendMessage(companionExtensionId, { message: MESSAGE_TYPES.TOKEN, token });
     }
 };
 
 export const sendFavoritesToExtension = (favorites: any[]): void => {
-    if (chrome && chrome.runtime && chrome.runtime.sendMessage) {
+    if (window.chrome && chrome.runtime && chrome.runtime.sendMessage) {
         chrome.runtime.sendMessage(companionExtensionId, { message: MESSAGE_TYPES.FAVORITES, favorites });
     }
 };
