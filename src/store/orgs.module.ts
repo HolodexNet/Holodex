3244e9ee79b7b9e3acb17a428523ac718f93e0e3
David Chen <3145205+RiceCakess@users.noreply.github.com>
2021-09-08 19:12:41 -0700

    Optimize orgs.json handling

--- src/store/orgs.module.ts
+++ src/store/orgs.module.ts
@@ -1,86 +1,35 @@
-// /* eslint-disable no-shadow */
-// import api from "@/utils/backend-api";
+/* eslint-disable no-shadow */
+import api from "@/utils/backend-api";
 
-// const initialState = {
-//     orgsPrefixes: {
-//         "Atelier Live": "Atelier",
-//         Chukorara: "Chuko",
-//         "Eilene Family": "Eilene",
-//         "Hanayori Joshiryo": "Hana",
-//         Hololive: "Holo",
-//         Independents: "Indie",
-//         "Kizuna Ai Inc.": "Kizuna",
-//         Marbl_s: "Marbl",
-//         Nijisanji: "Niji",
-//         Masquerade: "Masq",
-//         "Riot Music": "Riot",
-//         "Nori Pro": "Nori",
-//         "Aogiri Highschool": "Aogiri",
-//         KAMITSUBAKI: "KT",
-//         "VOICE-ORE": "V.O.",
-//         "Hoshimeguri Gakuen": "Stellar",
-//         "V Dimension.Creators": "VDC",
-//         Tsunderia: "Tsun",
-//         "Unreal Night Girls": "UNG",
-//         "X enc'ount": "X'",
-//         "Yuni Create": "Yuni",
-//         "All Vtubers": "Vtuber",
-//     },
-//     isLoading: true,
-// };
+const initialState = {
+    orgs: [],
+};
 
-// export const state = { ...initialState };
+export const state = { ...initialState };
 
-// const getters = {
-//     getLogoPrefix(state) {
-//         return (name) =>
-//             (state.orgsPrefixes[name] || name);
+const getters = {};
 
-//     }
-// };
+const actions = {
+    async fetchOrgs({ commit }) {
+        commit("setOrgs", [
+            ...(this.hideAllVTubers ? [] : [{ name: "All Vtubers", short: "Vtuber", name_jp: null }]),
+            ...(await api.orgs()).data.sort(
+                (a, b) => a.name.toLowerCase().charCodeAt(0) - b.name.toLowerCase().charCodeAt(0),
+            ),
+        ]);
+    },
+};
 
-// const actions = {
-//     fetchOrgs({ state, commit }) {
+const mutations = {
+    setOrgs(state, orgs) {
+        state.orgs = orgs;
+    },
+};
 
-//         commit("fetchStart");
-//         return api
-//             .orgs()
-//             .then(({ data }) => {
-//                 commit("setOrgs", data);
-//                 commit("fetchEnd");
-//             })
-//             .catch((e) => {
-//                 console.error(e);
-//             });
-//     },
-// };
-
-// const mutations = {
-//     fetchStart(state) {
-//         state.isLoading = true;
-//     },
-//     fetchEnd(state) {
-//         state.isLoading = false;
-//     },
-//     fetchError(state) {
-//         state.hasError = true;
-//         state.isLoading = false;
-//     },
-//     setId(state, id) {
-//         state.id = id;
-//     },
-//     setOrgs(state, channel) {
-//         state.channel = channel;
-//     },
-//     resetState(state) {
-//         Object.assign(state, initialState);
-//     },
-// };
-
-// export default {
-//     namespaced: true,
-//     state,
-//     getters,
-//     actions,
-//     mutations,
-// };
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations,
+};
