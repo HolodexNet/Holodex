614e6ebd125fcb13cb4c992e10198ca3b4cb6f30
David Chen <3145205+RiceCakess@users.noreply.github.com>
2021-09-12 02:58:02 -0700

    Add hints and i18n

--- src/views/MultiView.vue
+++ src/views/MultiView.vue
@@ -21,7 +21,7 @@
               icon
               v-on="on"
             >
-              <v-icon>{{ icons.mdiGridLarge, }}</v-icon>
+              <v-icon>{{ icons.mdiGridLarge }}</v-icon>
             </v-btn>
           </template>
           <portal to="preset-dialog" :disabled="!showPresetSelector">
@@ -61,6 +61,23 @@
           <div style="display: inline-block; margin-right: 10px">
             <div style="height: 10vh; border: 1px solid gray; width: 1px; margin-left: 95%; margin-top: 28px" />
             {{ $t("views.multiview.openToolbarTip") }}
+          </div>
+        </div>
+        <div style="padding-top: 10vh" class="d-flex justify-center">
+          <div class="hints">
+            <div class="text-h4">
+              {{ $t("views.multiview.hints") }}
+            </div>
+            <div>1. <v-icon>{{ icons.mdiGridLarge }}</v-icon> {{ $t("views.multiview.presetsHint") }}</div>
+            <!-- <div>2. <v-icon>{{ mdiContentSave }}</v-icon> Customize your experience by creating and saving layouts</div> -->
+            <div>
+              2.
+              <v-icon>{{ mdiTuneVertical }}</v-icon>
+              {{ $t("views.multiview.mediaControlsHint1") }}
+              <v-icon>{{ mdiFastForward }}</v-icon>
+              {{ $t("views.multiview.mediaControlsHint2") }}
+            </div>
+            <div>3. <v-icon>{{ mdiCardPlus }}</v-icon> {{ $t("views.multiview.dragDropHint") }}</div>
           </div>
         </div>
       </template>
@@ -207,6 +224,8 @@ export default {
             mdiCardPlus,
             mdiPause,
             mdiFastForward,
+            mdiTuneVertical,
+            mdiContentSave,
 
             showSelectorForId: -1,
 
@@ -486,5 +505,11 @@ export default {
 
 .vue-grid-layout {
     transition: none;
+}
+
+.hints {
+    div {
+        margin-bottom: 10px;
+    }
 }
 </style>
