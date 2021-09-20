81004ab1785be8e666056422b8928dbab991b6e5
Shiaupiau <stu43005@gmail.com>
2021-09-20 15:14:46 +0800

    Fix orgs json url

--- src/utils/backend-api.ts
+++ src/utils/backend-api.ts
@@ -16,7 +16,7 @@ export const axiosInstance = (() => {
 export default {
     orgs() {
         // Use fetch api to take advantage of pre-fetch
-        return fetch(`${API_BASE_URL}/orgs.json`).then((r) => r.json());
+        return fetch(`${SITE_BASE_URL}/orgs.json`).then((r) => r.json());
     },
     stats() {
         return axiosInstance({ url: "stats.json", baseURL: SITE_BASE_URL });
