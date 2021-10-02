5a6ed7a23482ded7f9400c084f38c67f1119e58a
Shiaupiau <stu43005@gmail.com>
2021-10-02 18:47:17 +0800

    Fix artwork resizing

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -3,7 +3,7 @@ import { langs } from "@/plugins/vuetify";
 
 export function resizeArtwork(artworkUrl, size = 400) {
     // https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/9c/39/27/9c392780-3f34-d322-9dde-002618154f40/source/400x400bb.jpg
-    const adjustedSize = window.devicePixelRatio * size;
+    const adjustedSize = Math.floor(window.devicePixelRatio * size);
     const match = /^https:\/\/(.+?)\.mzstatic\.com\/image\/thumb\/(.+?)\/source\//.exec(artworkUrl);
     if (!match) return artworkUrl;
     const serv = match[1];
