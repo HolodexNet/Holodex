f849bd4115f2b107e3a1165f13bd0ede3780901a
David Chen <3145205+RiceCakess@users.noreply.github.com>
2021-10-12 23:21:53 -0700

    Clean up multiview background

--- src/views/MultiView.vue
+++ src/views/MultiView.vue
@@ -36,52 +36,13 @@
     </MultiviewToolbar>
 
     <!-- Multiview Cell Area Background -->
-    <div
-      class="mv-background"
-      :style="{
-        'background-size': `${columnWidth}px ${rowHeight}px`,
-        height: `calc(100% - ${collapseToolbar ? 0 : 64}px)`,
-        top: `${collapseToolbar ? 0 : 64}px`,
-      }"
-    >
-      <template v-if="layout.length === 0">
-        <div v-if="!collapseToolbar" style="max-width: 50%; display: inline-block">
-          <div style="display: inline-block; margin-right: 20px; margin-left: 10px">
-            <div style="height: 10vh; border: 1px solid gray; width: 1px; margin-left: 50%" />
-            {{ $t("views.multiview.autoLayoutTip") }}
-          </div>
-        </div>
-        <div v-if="!collapseToolbar" style="max-width: 50%; display: inline-block; float: right">
-          <div style="display: inline-block; margin-right: 10px">
-            <div style="height: 10vh; border: 1px solid gray; width: 1px; margin-left: 50%" />
-            {{ $t("views.multiview.createLayoutTip") }}
-          </div>
-        </div>
-        <div v-if="collapseToolbar" style="max-width: 50%; display: inline-block; float: right">
-          <div style="display: inline-block; margin-right: 10px">
-            <div style="height: 10vh; border: 1px solid gray; width: 1px; margin-left: 95%; margin-top: 28px" />
-            {{ $t("views.multiview.openToolbarTip") }}
-          </div>
-        </div>
-        <div style="padding-top: 10vh" class="d-flex justify-center">
-          <div class="hints">
-            <div class="text-h4">
-              {{ $t("views.multiview.hints") }}
-            </div>
-            <div>1. <v-icon>{{ icons.mdiGridLarge }}</v-icon> {{ $t("views.multiview.presetsHint") }}</div>
-            <!-- <div>2. <v-icon>{{ mdiContentSave }}</v-icon> Customize your experience by creating and saving layouts</div> -->
-            <div>
-              2.
-              <v-icon>{{ mdiTuneVertical }}</v-icon>
-              {{ $t("views.multiview.mediaControlsHint1") }}
-              <v-icon>{{ mdiFastForward }}</v-icon>
-              {{ $t("views.multiview.mediaControlsHint2") }}
-            </div>
-            <div>3. <v-icon>{{ mdiCardPlus }}</v-icon> {{ $t("views.multiview.dragDropHint") }}</div>
-          </div>
-        </div>
-      </template>
-    </div>
+    <multiview-background
+      :show-tips="layout.length === 0"
+      :column-width="columnWidth"
+      :row-height="rowHeight"
+      :collapse-toolbar="collapseToolbar"
+    />
+
     <!-- Floating button to open toolbar when collapsed -->
     <v-btn
       v-if="collapseToolbar"
@@ -176,7 +137,6 @@
 
 <script lang="ts">
 import { GridLayout, GridItem } from "@/external/vue-grid-layout/src/components/index";
-// import Cell from "@/components/multiview/Cell.vue";
 import MediaControls from "@/components/multiview/MediaControls.vue";
 import EmptyCell from "@/components/multiview/EmptyCell.vue";
 import VideoCell from "@/components/multiview/VideoCell.vue";
@@ -188,8 +148,9 @@ import MultiviewToolbar from "@/components/multiview/MultiviewToolbar.vue";
 import MultiviewLayoutMixin from "@/components/multiview/MultiviewLayoutMixin";
 import LayoutChangePrompt from "@/components/multiview/LayoutChangePrompt.vue";
 import VideoSelector from "@/components/multiview/VideoSelector.vue";
+import MultiviewBackground from "@/components/multiview/MultiviewBackground.vue";
 import {
-    mdiViewGridPlus, mdiCardPlus, mdiContentSave, mdiPause, mdiTuneVertical, mdiFastForward,
+    mdiViewGridPlus, mdiCardPlus, mdiContentSave, mdiTuneVertical,
 } from "@mdi/js";
 import { decodeLayout } from "@/utils/mv-utils";
 import { mapState, mapGetters } from "vuex";
@@ -210,6 +171,7 @@ export default {
         LayoutChangePrompt,
         CellContainer,
         MediaControls,
+        MultiviewBackground,
     },
     mixins: [MultiviewLayoutMixin],
     metaInfo() {
@@ -223,8 +185,6 @@ export default {
     data() {
         return {
             mdiCardPlus,
-            mdiPause,
-            mdiFastForward,
             mdiTuneVertical,
             mdiContentSave,
 
@@ -260,15 +220,6 @@ export default {
                     color: "red",
                     collapse: true,
                 },
-                // {
-                //     icon: this.icons.mdiGridLarge,
-                //     tooltip: this.$t("views.multiview.presets"),
-                //     onClick: () => {
-                //         this.showPresetSelectorMenu = true;
-                //     },
-                //     color: "primary",
-                //     collapse: true,
-                // },
                 {
                     icon: mdiContentSave,
                     onClick: () => {
@@ -283,7 +234,6 @@ export default {
                     tooltip: this.$t("views.multiview.mediaControls"),
                     color: "orange",
                     onClick: () => {
-                        // this.setMuteAll(true);
                         this.showMediaControls = !this.showMediaControls;
                     },
                 },
@@ -432,14 +382,6 @@ export default {
 </script>
 
 <style lang="scss">
-.mv-background {
-    opacity: 0.75;
-    position: absolute;
-    width: 100%;
-    background-repeat: initial;
-    background-image: linear-gradient(to right, rgba(128, 128, 128, 0.15) 1px, transparent 1px),
-        linear-gradient(to bottom, rgba(128, 128, 128, 0.15) 1px, transparent 1px);
-}
 .mobile-helpers {
     -webkit-user-select: none;
     -khtml-user-select: none;
