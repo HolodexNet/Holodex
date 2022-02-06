e003fe0b580d3f6debc780a0b27ec4694ee0d232
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2022-02-06 15:03:59 +0100

    add unit tests with jest and esbuild

---
+++ tests/example.spec.ts
@@ -0,0 +1,30 @@
+import { shallowMount } from "@vue/test-utils";
+import music from "../src/store/music.module";
+/*
+import watch from "@/store/watch.module";
+import Stats from "@/components/common/Stats.vue";
+import SearchBar from "@/components/common/SearchBar.vue";
+*/
+
+describe("example", () => {
+  it("should test stuff", () => {
+    /*
+    const wrapper = shallowMount(SearchBar, {
+      mocks: {
+        $store: {
+          state: {
+            blargh: "honk",
+          },
+        },
+        $route: {},
+        $t: () => {},
+      },
+    });
+
+    expect(SearchBar.data().isLoading).toEqual(false);
+    */
+    const state = {}
+    music.mutations.openBar(state)
+    expect(state).toEqual({ isOpen: true })
+  });
+});
