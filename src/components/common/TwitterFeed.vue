888a38c74e3bde570086a7695fbc8e9fc5af12c2
teduniq <reyestedowen3@gmail.com>
2022-05-14 01:16:18 +0800

    style twitter embed

--- src/components/common/TwitterFeed.vue
+++ src/components/common/TwitterFeed.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- <div class="ma-auto"> -->
   <a
     class="twitter-timeline"
     data-dnt="true"
@@ -9,9 +8,27 @@
   >
     Tweets by @holodex
   </a>
-  <!-- </div> -->
 </template>
 <script lang="ts">
+function waitForElement(selector) {
+    return new Promise((resolve) => {
+        if (document.querySelector(selector)) {
+            resolve(document.querySelector(selector));
+            return;
+        }
+        const observer = new MutationObserver(() => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true,
+        });
+    });
+}
+
 export default {
     mounted() {
         // TODO(jprochazk): declare this in globals.d.ts
@@ -24,6 +41,35 @@ export default {
         } else {
             w.twttr.widgets.load();
         }
+        // style twitter iframe
+        waitForElement(".twitter-timeline-rendered").then((el) => {
+            const twitterIframe: any = el as HTMLIFrameElement;
+            setTimeout(() => {
+                // switch position of title and info circle in DOM in order for flex to work
+                const timelineHeader = twitterIframe.contentDocument.querySelector(".timeline-Header");
+                const headerTitle = twitterIframe.contentDocument.querySelector(".timeline-Header-title");
+                const headerInfoCircle = twitterIframe.contentDocument.querySelector(".timeline-InformationCircle");
+                timelineHeader.insertBefore(headerTitle, headerInfoCircle);
+                // remove embed link
+                twitterIframe.contentDocument.querySelector(".u-floatLeft").remove();
+                let twitterStyles = " * { scrollbar-color: #cdcdcd transparent; scrollbar-width: thin; }"
+                    + " ::-webkit-scrollbar { width: 8px; }"
+                    + " ::-webkit-scrollbar-thumb { background-color: #686868; border-radius: 5px; }"
+                    + " ::-webkit-scrollbar-thumb:hover { background-color: #505050; }"
+                    + " ::-webkit-scrollbar-track { background-color: transparent; }"
+                    + " .timeline-Header { padding: 16px !important; display: flex;"
+                    + "  justify-content: space-between; align-items: center; }"
+                    + " .timeline-InformationCircle { position: unset !important; }";
+                if (this.$vuetify.theme.dark) {
+                    const darkStyles = " * { scrollbar-color: #686868 transparent; scrollbar-width: thin; }"
+                        + " .timeline-Widget { background-color: #1e1e1e !important; }"
+                        + " .timeline-Tweet:hover { background-color: #303030 !important; }";
+                    twitterStyles += darkStyles;
+                }
+                // insert styles in <style> tag
+                twitterIframe.contentDocument.all[2].innerHTML += twitterStyles;
+            }, 600);
+        });
     },
 };
 </script>
