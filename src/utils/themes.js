b51db7d819302e86938dc62e2ed67cbeb559c437
Yasuaki Uechi <y@uechi.io>
2022-05-16 22:52:58 +0900

    Kobo theme

--- src/utils/themes.js
+++ src/utils/themes.js
@@ -1,325 +1,341 @@
 export default [
-    {
-        name: "Aqua",
-        id: 0,
-        themes: {
-            dark: {
-                background: "#121212",
-                primary: "#F06292",
-                secondary: "#3b88d5",
-            },
-            light: {
-                background: "#f2f2f2",
-                primary: "#F06292",
-                secondary: "#64B5F6",
-            },
-        },
+  {
+    name: "Aqua",
+    id: 0,
+    themes: {
+      dark: {
+        background: "#121212",
+        primary: "#F06292",
+        secondary: "#3b88d5",
+      },
+      light: {
+        background: "#f2f2f2",
+        primary: "#F06292",
+        secondary: "#64B5F6",
+      },
     },
-    {
-        name: "Matsuri",
-        id: 1,
-        themes: {
-            dark: {
-                background: "#121212",
-                primary: "#faa749", // #F4B056
-                secondary: "#82D251",
-            },
-            light: {
-                background: "#f2f2f2",
-                secondary: "#839C35",
-                primary: "#F2AD46",
-            },
-        },
+  },
+  {
+    name: "Matsuri",
+    id: 1,
+    themes: {
+      dark: {
+        background: "#121212",
+        primary: "#faa749", // #F4B056
+        secondary: "#82D251",
+      },
+      light: {
+        background: "#f2f2f2",
+        secondary: "#839C35",
+        primary: "#F2AD46",
+      },
     },
-    {
-        name: "Korone",
-        id: 2,
-        themes: {
-            // https://coolors.co/a8736d-f4dba2-f4d7cd-93d3ce-e46d8d <-- Korone color swatch
-            dark: {
-                background: "#0D0707",
-                primary: "#B07975",
-                secondary: "#D57E3D",
-            },
-            light: {
-                background: "#f2f2f2",
-                primary: "#F6D68D",
-                secondary: "#80CDC7",
-            },
-        },
+  },
+  {
+    name: "Korone",
+    id: 2,
+    themes: {
+      // https://coolors.co/a8736d-f4dba2-f4d7cd-93d3ce-e46d8d <-- Korone color swatch
+      dark: {
+        background: "#0D0707",
+        primary: "#B07975",
+        secondary: "#D57E3D",
+      },
+      light: {
+        background: "#f2f2f2",
+        primary: "#F6D68D",
+        secondary: "#80CDC7",
+      },
     },
-    {
-        name: "Rushia",
-        id: 3,
-        themes: {
-            dark: {
-                background: "#121212",
-                primary: "#22C4AC",
-                secondary: "#5783BF",
-            },
-            light: {
-                background: "#f2f2f2",
-                primary: "#9E65A6",
-                secondary: "#F2C9EB",
-            },
-        },
+  },
+  {
+    name: "Rushia",
+    id: 3,
+    themes: {
+      dark: {
+        background: "#121212",
+        primary: "#22C4AC",
+        secondary: "#5783BF",
+      },
+      light: {
+        background: "#f2f2f2",
+        primary: "#9E65A6",
+        secondary: "#F2C9EB",
+      },
     },
-    {
-        name: "Fubuki",
-        id: 4,
-        themes: {
-            dark: {
-                background: "#0C111A",
-                primary: "#9E8461",
-                secondary: "#83B0BD",
-            },
-            light: {
-                background: "#FDFDFD",
-                primary: "#317192",
-                secondary: "#83B0BD",
-            },
-        },
+  },
+  {
+    name: "Fubuki",
+    id: 4,
+    themes: {
+      dark: {
+        background: "#0C111A",
+        primary: "#9E8461",
+        secondary: "#83B0BD",
+      },
+      light: {
+        background: "#FDFDFD",
+        primary: "#317192",
+        secondary: "#83B0BD",
+      },
     },
-    {
-        name: "Figaro",
-        id: 5,
-        themes: {
-            dark: {
-                background: "#121212",
-                primary: "#8FB5B7",
-                secondary: "#688BA1",
-            },
-            light: {
-                background: "#F5F5F5",
-                primary: "#688BA1",
-                secondary: "#8FB5B7",
-            },
-        },
+  },
+  {
+    name: "Figaro",
+    id: 5,
+    themes: {
+      dark: {
+        background: "#121212",
+        primary: "#8FB5B7",
+        secondary: "#688BA1",
+      },
+      light: {
+        background: "#F5F5F5",
+        primary: "#688BA1",
+        secondary: "#8FB5B7",
+      },
     },
-    {
-        name: "Shion",
-        id: 6,
-        themes: {
-            dark: {
-                background: "#121212",
-                primary: "#998AEB",
-                secondary: "#684C7C",
-            },
-            light: {
-                background: "#F2F2F2",
-                primary: "#684C7C",
-                secondary: "#998AEB",
-            },
-        },
+  },
+  {
+    name: "Shion",
+    id: 6,
+    themes: {
+      dark: {
+        background: "#121212",
+        primary: "#998AEB",
+        secondary: "#684C7C",
+      },
+      light: {
+        background: "#F2F2F2",
+        primary: "#684C7C",
+        secondary: "#998AEB",
+      },
     },
-    {
-        name: "Coco",
-        id: 7,
-        themes: {
-            dark: {
-                background: "#121212",
-                primary: "#D3B633",
-                secondary: "#FF9A63",
-                success: "#B333C9",
-            },
-            light: {
-                background: "#f2f2f2",
-                secondary: "#B333C9",
-                primary: "#FF9257",
-            },
-        },
+  },
+  {
+    name: "Coco",
+    id: 7,
+    themes: {
+      dark: {
+        background: "#121212",
+        primary: "#D3B633",
+        secondary: "#FF9A63",
+        success: "#B333C9",
+      },
+      light: {
+        background: "#f2f2f2",
+        secondary: "#B333C9",
+        primary: "#FF9257",
+      },
     },
-    {
-        name: "Suisei",
-        id: 8,
-        themes: {
-            dark: {
-                background: "#121212",
-                primary: "#a8d6fc",
-                secondary: "#3c3a97",
-                success: "#16f7a5",
-            },
-            light: {
-                background: "#f2f2f2",
-                secondary: "#a8d6fc",
-                primary: "#3c3a97",
-            },
-        },
+  },
+  {
+    name: "Suisei",
+    id: 8,
+    themes: {
+      dark: {
+        background: "#121212",
+        primary: "#a8d6fc",
+        secondary: "#3c3a97",
+        success: "#16f7a5",
+      },
+      light: {
+        background: "#f2f2f2",
+        secondary: "#a8d6fc",
+        primary: "#3c3a97",
+      },
     },
-    {
-        name: "Haato",
-        id: 9,
-        themes: {
-            dark: {
-                background: "#121212",
-                primary: "#ffe799",
-                secondary: "#d52d43",
-            },
-            light: {
-                background: "#f2f2f2",
-                secondary: "#ffe799",
-                primary: "#d52d43",
-            },
-        },
+  },
+  {
+    name: "Haato",
+    id: 9,
+    themes: {
+      dark: {
+        background: "#121212",
+        primary: "#ffe799",
+        secondary: "#d52d43",
+      },
+      light: {
+        background: "#f2f2f2",
+        secondary: "#ffe799",
+        primary: "#d52d43",
+      },
     },
-    {
-        name: "Miko",
-        id: 10,
-        themes: {
-            dark: {
-                background: "#121212",
-                primary: "#fbb3a5",
-                secondary: "#fe7c8b",
-            },
-            light: {
-                background: "#f2f2f2",
-                secondary: "#fe7c8b",
-                primary: "#fbb3a5",
-            },
-        },
+  },
+  {
+    name: "Miko",
+    id: 10,
+    themes: {
+      dark: {
+        background: "#121212",
+        primary: "#fbb3a5",
+        secondary: "#fe7c8b",
+      },
+      light: {
+        background: "#f2f2f2",
+        secondary: "#fe7c8b",
+        primary: "#fbb3a5",
+      },
     },
-    {
-        name: "IRyS",
-        id: 11,
-        themes: {
-            dark: {
-                background: "#121212",
-                primary: "#d91a5d",
-                secondary: "#98395c",
-            },
-            light: {
-                background: "#f2f2f2",
-                secondary: "#58294c ",
-                primary: "#d91a5d",
-            },
-        },
+  },
+  {
+    name: "IRyS",
+    id: 11,
+    themes: {
+      dark: {
+        background: "#121212",
+        primary: "#d91a5d",
+        secondary: "#98395c",
+      },
+      light: {
+        background: "#f2f2f2",
+        secondary: "#58294c ",
+        primary: "#d91a5d",
+      },
     },
-    {
-        name: "Luna",
-        id: 12,
-        themes: {
-            dark: {
-                background: "#121212",
-                primary: "#EB8FE1",
-                secondary: "#F7638B",
-            },
-            light: {
-                background: "#f3efef",
-                secondary: "#DB8FEB",
-                primary: "#F7638B",
-            },
-        },
+  },
+  {
+    name: "Luna",
+    id: 12,
+    themes: {
+      dark: {
+        background: "#121212",
+        primary: "#EB8FE1",
+        secondary: "#F7638B",
+      },
+      light: {
+        background: "#f3efef",
+        secondary: "#DB8FEB",
+        primary: "#F7638B",
+      },
     },
-    {
-        name: "Pekora",
-        id: 13,
-        themes: {
-            dark: {
-                background: "#121212",
-                primary: "#DC8C2C",
-                secondary: "#439369",
-            },
-            light: {
-                background: "#f2f2f2",
-                primary: "#DC8C2C",
-                secondary: "#439369",
-            },
-        },
+  },
+  {
+    name: "Pekora",
+    id: 13,
+    themes: {
+      dark: {
+        background: "#121212",
+        primary: "#DC8C2C",
+        secondary: "#439369",
+      },
+      light: {
+        background: "#f2f2f2",
+        primary: "#DC8C2C",
+        secondary: "#439369",
+      },
     },
-    {
-        name: "Polka",
-        id: 14,
-        themes: {
-            dark: {
-                background: "#121212",
-                primary: "#e9d9a3",
-                secondary: "#7f796a",
-            },
-            light: {
-                background: "#f2f2f2",
-                secondary: "#7f796a ",
-                primary: "#D4C378",
-            },
-        },
+  },
+  {
+    name: "Polka",
+    id: 14,
+    themes: {
+      dark: {
+        background: "#121212",
+        primary: "#e9d9a3",
+        secondary: "#7f796a",
+      },
+      light: {
+        background: "#f2f2f2",
+        secondary: "#7f796a ",
+        primary: "#D4C378",
+      },
     },
-    {
-        name: "Towa",
-        id: 15,
-        themes: {
-            dark: {
-                background: "#121212",
-                primary: "#e0e187",
-                secondary: "#7e6bac",
-            },
-            light: {
-                background: "#f2f2f2",
-                secondary: "#e0e187",
-                primary: "#7e6bac",
-            },
-        },
+  },
+  {
+    name: "Towa",
+    id: 15,
+    themes: {
+      dark: {
+        background: "#121212",
+        primary: "#e0e187",
+        secondary: "#7e6bac",
+      },
+      light: {
+        background: "#f2f2f2",
+        secondary: "#e0e187",
+        primary: "#7e6bac",
+      },
     },
-    {
-        name: "Flare",
-        id: 16,
-        themes: {
-            dark: {
-                background: "#121212",
-                primary: "#d79e69",
-                secondary: "#244f6f",
-            },
-            light: {
-                background: "#f2f2f2",
-                primary: "#d79e69",
-                secondary: "#244f6f",
-            },
-        },
+  },
+  {
+    name: "Flare",
+    id: 16,
+    themes: {
+      dark: {
+        background: "#121212",
+        primary: "#d79e69",
+        secondary: "#244f6f",
+      },
+      light: {
+        background: "#f2f2f2",
+        primary: "#d79e69",
+        secondary: "#244f6f",
+      },
     },
-    {
-        name: "Noel",
-        id: 17,
-        themes: {
-            dark: {
-                background: "#121212",
-                primary: "#9ba4b1",
-                secondary: "#928c97",
-            },
-            light: {
-                background: "#f2f2f2",
-                primary: "#9ba4b1",
-                secondary: "#928c97",
-            },
-        },
+  },
+  {
+    name: "Noel",
+    id: 17,
+    themes: {
+      dark: {
+        background: "#121212",
+        primary: "#9ba4b1",
+        secondary: "#928c97",
+      },
+      light: {
+        background: "#f2f2f2",
+        primary: "#9ba4b1",
+        secondary: "#928c97",
+      },
     },
-    {
-        name: "Lamy",
-        id: 18,
-        themes: {
-            dark: {
-                background: "#121212",
-                primary: "#338bcc",
-                secondary: "#72b0e4",
-            },
-            light: {
-                background: "#f2f2f2",
-                primary: "#338bcc",
-                secondary: "#72b0e4",
-            },
-        },
+  },
+  {
+    name: "Lamy",
+    id: 18,
+    themes: {
+      dark: {
+        background: "#121212",
+        primary: "#338bcc",
+        secondary: "#72b0e4",
+      },
+      light: {
+        background: "#f2f2f2",
+        primary: "#338bcc",
+        secondary: "#72b0e4",
+      },
     },
-    {
-        name: "Okayu",
-        id: 19,
-        themes: {
-            dark: {
-                background: "#121212",
-                primary: "#cda4d5",
-                secondary: "#4a477c",
-            },
-            light: {
-                background: "#f2f2f2",
-                primary: "#4a477c",
-                secondary: "#cda4d5",
-            },
-        },
+  },
+  {
+    name: "Okayu",
+    id: 19,
+    themes: {
+      dark: {
+        background: "#121212",
+        primary: "#cda4d5",
+        secondary: "#4a477c",
+      },
+      light: {
+        background: "#f2f2f2",
+        primary: "#4a477c",
+        secondary: "#cda4d5",
+      },
     },
+  },
+  {
+    name: "Kobo",
+    id: 20,
+    themes: {
+      dark: {
+        background: "#151515",
+        primary: "#7fbde2",
+        secondary: "#8ec7e5",
+      },
+      light: {
+        background: "#edf1f4",
+        primary: "#7fbde2",
+        secondary: "#9fcbee",
+      },
+    },
+  },
 ];
