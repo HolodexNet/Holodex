c60c9ea0c8267fbc82a75be40365135a100d2bc4
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-06-08 01:01:37 -0700

    quasar app package

--- package/quasar/src/App.vue
+++ package/quasar/src/App.vue
@@ -1,56 +1,67 @@
 <template>
-  <div class="q-pa-md">
-    <q-layout
-      view="hHh Lpr lff"
-      style="height: 100vh"
-      class="shadow-2 rounded-borders"
-    >
-      <q-header elevated class="bg-black">
-        <q-toolbar>
-          <!-- <q-btn flat @click="drawer = !drawer" round dense icon="menu" /> -->
-          <q-toolbar-title>ImagenAI CRM</q-toolbar-title>
-        </q-toolbar>
-      </q-header>
+  <q-layout view="hhh Lpr fFf">
 
-      <q-drawer
-        v-model="drawer"
-        show-if-above
-        :width="200"
-        :breakpoint="500"
-        bordered
-        class="bg-grey-3"
-      >
-        <q-scroll-area class="fit">
-          <q-list>
-            <template v-for="(menuItem, index) in menuList" :key="index">
-              <q-item
-                clickable
-                :to="menuItem.link"
-                :active="menuItem.label === 'Outbox'"
-              >
-                <q-item-section avatar>
-                  <q-icon :name="menuItem.icon" />
-                </q-item-section>
-                <q-item-section>
-                  {{ menuItem.label }}
-                </q-item-section>
-              </q-item>
-              <q-separator v-if="menuItem.separator" :key="'sep' + index" />
-            </template>
-          </q-list>
-        </q-scroll-area>
-      </q-drawer>
+    <q-header class="bg-primary text-white" height-hint="30">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
-      <q-page-container>
-        <q-page padding>
-          <router-view />
-        </q-page>
-      </q-page-container>
-    </q-layout>
-  </div>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/page1" label="Page One" />
+        <q-route-tab to="/page2" label="Page Two" />
+        <q-route-tab to="/page3" label="Page Three" />
+      </q-tabs>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <q-scroll-area class="fit">
+        <q-list>
+          <template v-for="(menuItem, index) in menuList" :key="index">
+            <q-item clickable :to="menuItem.link" :active="menuItem.label === 'Outbox'">
+              <q-item-section avatar>
+                <q-icon :name="menuItem.icon" />
+              </q-item-section>
+              <q-item-section>
+                {{ menuItem.label }}
+              </q-item-section>
+            </q-item>
+            <q-separator v-if="menuItem.separator" :key="'sep' + index" />
+          </template>
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
+
+    <q-page-container>
+      <q-page padding>
+        <router-view />
+      </q-page>
+    </q-page-container>
+
+    <q-footer reveal elevated class="bg-grey-8 text-white" v-model="$q.screen.lt.md">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
 </template>
 
 <script setup lang="ts">
+import { useQuasar } from "quasar";
 import { ref } from "vue";
 
 const menuList = [
@@ -99,7 +110,17 @@ defineProps({
   msg: String,
 });
 
-const drawer = ref(false);
+const leftDrawerOpen = ref(false)
+const rightDrawerOpen = ref(false)
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+function toggleRightDrawer() {
+  rightDrawerOpen.value = !rightDrawerOpen.value
+}
+
+const $q = useQuasar()
 </script>
 
 <style scoped>
