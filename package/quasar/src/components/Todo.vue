5f765905e12fe61de73582445c068ba47411096a
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-06-08 00:17:04 -0700

    ... deleting seems relatively straightforward

--- package/quasar/src/components/Todo.vue
+++ package/quasar/src/components/Todo.vue
@@ -2,9 +2,9 @@
     <div>
         TODOs
     </div>
-    <div @click="() => store.addTodo({id: store.todos.length, title: `${new Date().toTimeString()} TODO`})">Add</div>
+    <div @click="() => store.addTodo({id: ((store.todos[store.todos.length - 1]?.id || 0) + 1), title: `${new Date().toTimeString()} TODO`})">Add</div>
     <ul v-for="todo in store.todos">
-        <li>{{todo.id}}: {{todo.title}}</li>
+        <li>{{todo.id}}: {{todo.title}} (<span @click="() => store.deleteTodo(todo.id)">click to delete</span>)</li>
     </ul>
 </template>
 
