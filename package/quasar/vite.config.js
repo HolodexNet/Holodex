5ec7c89a052dbcf57bc511e6fb28c551e0185cfc
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-06-09 05:19:19 -0700

    localization injected

--- package/quasar/vite.config.js
+++ package/quasar/vite.config.js
@@ -6,10 +6,14 @@ import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 import { ViteAliases } from "vite-aliases";
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import content from '@originjs/vite-plugin-content',
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    content({
+      // for converting yml, xml, ini, and other files to ES6 modules.
+    }),
     vue({
       template: { transformAssetUrls },
     }),
