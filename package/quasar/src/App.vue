b095c782429c1e5a73325ac63686b417bb7f1efe
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-06-10 05:27:26 -0700

    vue-query works perfectly

--- package/quasar/src/App.vue
+++ package/quasar/src/App.vue
@@ -1,11 +1,14 @@
 <template>
     <app-frame />
+
+    <VueQueryDevToolsPanel />
 </template>
 
 <script setup lang="ts">
 
 import { useI18nInitialization } from './hooks/i18n/useI18nInitialization'
 import { useThemeInitialization } from './hooks/theme-changer/useThemeInitialization'
+import { VueQueryDevToolsPanel } from "vue-query/devtools";
 
 // initializing setup for Holodex:
 // Steps:
--- package/quasar/src/components/Todo.vue
+++ package/quasar/src/components/Todo.vue
@@ -2,20 +2,35 @@
     <div>
         TODOs
     </div>
-    <div @click="() => store.addTodo({id: ((store.todos[store.todos.length - 1]?.id || 0) + 1), title: `${new Date().toTimeString()} TODO`})">Add</div>
+    <div
+        @click="() => store.addTodo({ id: ((store.todos[store.todos.length - 1]?.id || 0) + 1), title: `${new Date().toTimeString()} TODO` })">
+        Add</div>
     <ul v-for="todo in store.todos">
-        <li>{{todo.id}}: {{todo.title}} (<span @click="() => store.deleteTodo(todo.id)">click to delete</span>)</li>
+        <li>{{ todo.id }}: {{ todo.title }} (<span @click="() => store.deleteTodo(todo.id)">click to delete</span>)</li>
     </ul>
+    <div>
+        LA Current Time: {{ out }}
+    </div>
 </template>
 
 <script lang="ts">
 import { useTodoStore } from "@/stores/todo";
+import { useQuery } from "vue-query";
 
 export default defineComponent({
     setup() {
         const todos = useTodoStore();
 
-        return {store: todos,}
+        // testing vue-query cross tab pollination looks GOOD
+        const { isLoading, isError, data, error } = useQuery(['LA_Timezone'], async (e) => {
+
+            const resp = await fetch("https://hacker-news.firebaseio.com/v0/topstories.json");
+
+            // const resp = await fetch('http://worldtimeapi.org/api/timezone/America/Los_Angeles')
+            return (resp.body as any);
+        }, { staleTime: 20000, cacheTime: 30000 })
+
+        return { store: todos, isLoading, isError, out: data }
     },
 })
 </script>
