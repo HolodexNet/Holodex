cc15ec061c14a8c0289566cb45fd03d6a3741f67
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-06-10 04:29:39 -0700

    vue query init

--- package/quasar/src/setup/index.ts
+++ package/quasar/src/setup/index.ts
@@ -2,3 +2,4 @@
 
 export { setupPinia } from "./setupPinia"
 export { setupI18N } from "./setupI18N"
+export { queryClient, REGULAR_QUERY_OPTIONS, STATIC_QUERY_OPTIONS, getVueQueryPluginOptions } from './setupQueryPlugin'
---
+++ package/quasar/src/setup/setupQueryPlugin.ts
@@ -0,0 +1,42 @@
+import type { QueryClientConfig, QueryObserverOptions } from "react-query/types/core";
+import type { VueQueryPluginOptions } from "vue-query";
+import { QueryClient } from "vue-query";
+import { broadcastQueryClient } from 'react-query/broadcastQueryClient-experimental'
+import { createWebStoragePersistor } from 'react-query/createWebStoragePersistor-experimental'
+import { persistQueryClient } from 'react-query/persistQueryClient-experimental'
+
+export const REGULAR_QUERY_OPTIONS: QueryObserverOptions = {
+    refetchOnMount: true,
+    staleTime: 3 * 60 * 1000, // 3 mins
+    cacheTime: 10 * 60 * 1000, // 10 minutes
+    refetchOnWindowFocus: false,
+}
+
+export const STATIC_QUERY_OPTIONS: QueryObserverOptions = {
+    refetchOnMount: true,
+    staleTime: 60 * 60 * 1000, // 1 hr
+    cacheTime: 60 * 60 * 1000, // 1 hr
+    refetchOnWindowFocus: false,
+}
+
+
+const queryClientConfig: QueryClientConfig = {
+    defaultOptions: { queries: REGULAR_QUERY_OPTIONS },
+}
+
+export const queryClient = new QueryClient(queryClientConfig);
+
+broadcastQueryClient({ queryClient: queryClient, broadcastChannel: 'hQC' })
+
+if (typeof (Storage) !== "undefined") {
+    // Code for localStorage
+    const lsPersistor = createWebStoragePersistor({ storage: window.localStorage, key: 'hQCdb' })
+    persistQueryClient({ queryClient: queryClient, persistor: lsPersistor, buster: 'v0', maxAge: 1000 * 60 * 60 * 24 })
+}
+
+export function getVueQueryPluginOptions(): VueQueryPluginOptions {
+
+    return {
+        queryClient: queryClient
+    }
+}
