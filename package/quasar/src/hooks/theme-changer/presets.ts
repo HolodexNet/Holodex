06783277ed7944d17e297ea375dd11c5cce51c0e
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-06-11 11:14:11 -0700

    type correction

--- package/quasar/src/hooks/theme-changer/presets.ts
+++ package/quasar/src/hooks/theme-changer/presets.ts
@@ -1,17 +1,17 @@
 import { ThemeDefinition } from "vuetify";
 import { Theme } from "./helpers";
 
-export const presets: object & ThemeDefinition = [
+export const presets: [{ name: string } & ThemeDefinition] = [
     {
         name: 'aqua',
         dark: true,
         colors: {
-            background: '#FFFFFF',
-            surface: '#FFFFFF',
-            primary: '#6200EE',
-            secondary: '#03DAC6',
+            background: '#1f1f1f',
+            surface: '#3e2e2e',
+            primary: '#3b88d5',
+            secondary: '#F06292',
             error: '#B00020',
-            info: '#2196F3',
+            info: '#64B5F6',
             success: '#4CAF50',
             warning: '#FB8C00',
         }
