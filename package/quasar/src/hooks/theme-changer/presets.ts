6eb5de2fedb32e358782a08bf812c06549b787c0
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-06-11 16:06:54 -0700

    playing around with daisy

--- package/quasar/src/hooks/theme-changer/presets.ts
+++ package/quasar/src/hooks/theme-changer/presets.ts
@@ -18,3 +18,19 @@ export const presets: [Theme] = [
         }
     },
 ]
+
+/**
+ * Additional Defaults for DaisyUI that will be applied globally.
+ */
+export const DaisyDefaults = {
+    "--rounded-box": "0.25rem", // border radius rounded-box utility class, used in card and other large boxes
+    "--rounded-btn": "0.25rem", // border radius rounded-btn utility class, used in buttons and similar element
+    "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
+    "--animation-btn": "0.25s", // duration of animation when you click on button
+    "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
+    "--btn-text-case": "uppercase", // set default text transform for buttons
+    "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+    "--border-btn": "1px", // border width of buttons
+    "--tab-border": "1px", // border width of tabs
+    "--tab-radius": "0.5rem", // border radius of tabs
+}
