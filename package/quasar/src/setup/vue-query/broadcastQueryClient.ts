ae24a4828ad9e144638f3d262e589dad0a5a34d2
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-06-11 15:41:52 -0700

    chore: cleanup

--- package/quasar/src/setup/vue-query/broadcastQueryClient.ts
+++ package/quasar/src/setup/vue-query/broadcastQueryClient.ts
@@ -24,6 +24,7 @@ export function broadcastQueryClient({
     const queryCache = queryClient.getQueryCache()
 
     queryClient.getQueryCache().subscribe(queryEvent => {
+        if (!queryEvent) return
         if (transaction) {
             return
         }
