6300ecf87e882d85eda3a7f589e9fb331f2e0e80
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-06-12 04:33:16 -0700

    some hooks

---
+++ package/quasar/src/hooks/common/useVideoService.ts
@@ -0,0 +1,15 @@
+import { useLangStore } from "@/stores/lang";
+
+export function useVideo(video: Video) {
+    const langPrefs = useLangStore();
+
+    const preferredChannelName = computed((() => {
+        return langPrefs.preferredLocaleFn(video.channel.english_name, video.channel.name)
+    }))
+
+    const preferredTitle = computed((() => {
+        return langPrefs.preferredLocaleFn(video.title, video.jp_name)
+    }))
+
+    return { preferredTitle, preferredChannelName };
+}
---
+++ package/quasar/src/hooks/temporal/useFormattedTime.ts
@@ -0,0 +1,38 @@
+import { useLangStore } from "@/stores/lang";
+import { formatDistance } from "@/utils/time";
+import { useTimestamp } from "@vueuse/core";
+import { useI18n } from "vue-i18n";
+
+
+// export function useVideoTime(video: Video) {
+
+//     const langStore = useLangStore();
+//     const { t } = useI18n({ useScope: 'global' })
+//     const tick = useTimestamp({ interval: 5000 });
+
+//     const localized = computed(() => {
+//         const tickx = tick.value + 1;
+//         switch (video.status) {
+//             case "upcoming":
+//                 // print relative time in hours if less than 24 hours,
+//                 // print full date if greater than 24 hours
+//                 return formatDistance(
+//                     video.start_scheduled || video.available_at,
+//                     langStore.lang,
+//                     t,
+//                     false, // allowNegative = false
+//                 ); // upcoming videos don't get to be ("5 minutes ago")
+//             case "live":
+//                 return t("component.videoCard.liveNow");
+//             default:
+//                 return formatDistance(
+//                     video.available_at,
+//                     langStore.lang,
+//                     t,
+//                 );
+//         }
+
+//     })
+
+//     return localized
+// }
