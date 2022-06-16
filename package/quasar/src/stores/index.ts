4ac7d0b5af3900d1ac9962fb7955bebba12a1c7f
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-06-15 22:11:50 -0700

    stores

---
+++ package/quasar/src/stores/index.ts
@@ -0,0 +1,8 @@
+export { useLangStore } from "./lang";
+export { usePlaylistStore } from "./playlist";
+export { useSiteStore } from "./site";
+export { useWatchHistoryStore } from "./watchHistory";
+export { useThemeStore } from "./theme"
+export { useTLStore } from './tldex'
+
+//todo and user are just test stores.
