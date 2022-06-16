8945e4071034e338a7cac328adc7e99047a66072
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-06-15 21:48:58 -0700

    why's icon not working

--- package/quasar/src/setup/setupVuetify.ts
+++ package/quasar/src/setup/setupVuetify.ts
@@ -2,8 +2,8 @@ import { createVuetify } from "vuetify";
 
 
 export const vuetify = createVuetify({
-    theme: false,
+    // theme: false,
     icons: {
         defaultSet: 'svg',
-    },
+    } as any,
 })
