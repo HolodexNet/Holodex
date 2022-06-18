5bc67bb20ec5853c800c6d7aa4629ffcd4c4c42b
David Chen <3145205+RiceCakess@users.noreply.github.com>
2022-06-18 14:00:59 -0700

    Update eslint config

--- package/quasar/.eslintrc.js
+++ package/quasar/.eslintrc.js
@@ -5,27 +5,28 @@ module.exports = {
     "vue/setup-compiler-macros": true,
   },
   extends: [
-    'plugin:vue/essential',
-    'plugin:vue/vue3-recommended',
-    '@vue/typescript/recommended',
-    './.eslintrc-auto-import.json'
+    "plugin:vue/essential",
+    "plugin:vue/vue3-recommended",
+    "@vue/typescript/recommended",
+    "./.eslintrc-auto-import.json",
+    "prettier",
   ],
   parserOptions: {
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
     ecmaVersion: 2021,
-  },    // "parser": {
-    //   // Script parser for `<script>`
-    //   "js": "espree",
+  }, // "parser": {
+  //   // Script parser for `<script>`
+  //   "js": "espree",
 
-    //   // Script parser for `<script lang="ts">`
-    //   "ts": "@typescript-eslint/parser",
+  //   // Script parser for `<script lang="ts">`
+  //   "ts": "@typescript-eslint/parser",
 
-    //   // Script parser for vue directives (e.g. `v-if=` or `:attribute=`)
-    //   // and vue interpolations (e.g. `{{variable}}`).
-    //   // If not specified, the parser determined by `<script lang ="...">` is used.
-    //   "<template>": "espree",
-    // }
+  //   // Script parser for vue directives (e.g. `v-if=` or `:attribute=`)
+  //   // and vue interpolations (e.g. `{{variable}}`).
+  //   // If not specified, the parser determined by `<script lang ="...">` is used.
+  //   "<template>": "espree",
+  // }
   // },
 
   settings: {
@@ -37,29 +38,29 @@ module.exports = {
     },
   },
 
-  'rules': {
-    'brace-style': [2, '1tbs', { 'allowSingleLine': true }],
+  rules: {
+    "brace-style": [2, "1tbs", { allowSingleLine: true }],
 
-    "vue/script-indent": ["error", 4, { baseIndent: 0, switchCase: 1 }],
+    // "vue/script-indent": ["error", 4, { baseIndent: 0, switchCase: 1 }],
 
-    'vue/max-attributes-per-line': 0,
-    'vue/valid-v-for': 0,
+    "vue/max-attributes-per-line": 0,
+    "vue/valid-v-for": 0,
     "vue/no-multiple-template-root": 0,
 
     // allow async-await
-    'generator-star-spacing': 'off',
+    "generator-star-spacing": "off",
 
     // allow paren-less arrow functions
-    'arrow-parens': 0,
-    'one-var': 0,
+    "arrow-parens": 0,
+    "one-var": 0,
 
-    'import/first': 0,
-    'import/extensions': 0,
-    'import/no-unresolved': 0,
-    'import/no-extraneous-dependencies': 0,
+    "import/first": 0,
+    "import/extensions": 0,
+    "import/no-unresolved": 0,
+    "import/no-extraneous-dependencies": 0,
 
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
     "@typescript-eslint/no-explicit-any": 0,
-  }
+  },
 };
