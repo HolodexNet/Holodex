02f80e75d259e373fe61010e15016489f7565415
David Chen <3145205+RiceCakess@users.noreply.github.com>
2022-06-18 13:45:16 -0700

    ﻿Add pretteir for liniting

--- package/quasar/src/setup/index.ts
+++ package/quasar/src/setup/index.ts
@@ -1,5 +1,8 @@
-
-
-export { setupPinia } from "./setupPinia"
-export { setupI18N } from "./setupI18N"
-export { queryClient, REGULAR_QUERY_OPTIONS, STATIC_QUERY_OPTIONS, getVueQueryPluginOptions } from './setupQueryPlugin'
+export { setupPinia } from "./setupPinia";
+export { setupI18N } from "./setupI18N";
+export {
+  queryClient,
+  REGULAR_QUERY_OPTIONS,
+  STATIC_QUERY_OPTIONS,
+  getVueQueryPluginOptions,
+} from "./setupQueryPlugin";
