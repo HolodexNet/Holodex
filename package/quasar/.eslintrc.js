95b3e27a8af7cfc6e826ec268663751f32db0302
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-06-18 20:58:58 -0700

    err wrong path

--- package/quasar/.eslintrc.js
+++ package/quasar/.eslintrc.js
@@ -37,7 +37,7 @@ module.exports = {
       },
     },
   },
-  ignorePatterns: ["components.d.ts", "./src/auto-imports.d.ts"],
+  ignorePatterns: ["./components.d.ts", "./src/auto-imports.d.ts"],
 
   rules: {
     "brace-style": [2, "1tbs", { allowSingleLine: true }],
