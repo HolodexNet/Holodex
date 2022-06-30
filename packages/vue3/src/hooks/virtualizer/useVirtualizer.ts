1125c5a366366457e4d4c526a46379b7cf26bb1c
David Chen <3145205+RiceCakess@users.noreply.github.com>
2022-06-29 19:52:39 -0700

    Add useVirtualizer

--- packages/vue3/src/hooks/virtualizer/useVirtualizer.ts
+++ packages/vue3/src/hooks/virtualizer/useVirtualizer.ts
@@ -9,84 +9,52 @@ import {
   VirtualizerOptions,
   windowScroll,
 } from "@tanstack/virtual-core";
+import { Ref } from "vue";
 export * from "@tanstack/virtual-core";
 
 function useVirtualizerBase<TScrollElement, TItemElement = unknown>(
   options: VirtualizerOptions<TScrollElement, TItemElement>
-): Virtualizer<TScrollElement, TItemElement> {
-  const instance = new Virtualizer<TScrollElement, TItemElement>(options);
+): Ref<Virtualizer<TScrollElement, TItemElement>> {
+  const virtualizer = reactive(
+    new Virtualizer<TScrollElement, TItemElement>(options)
+  );
 
-  // the setup for the virtualizer, most crucially for us:
-  //
+  const rerenderRef = ref(0);
+
+  onMounted(() => {
+    virtualizer._didMount();
+    virtualizer._willUpdate();
+  });
+
+  virtualizer.setOptions({
+    ...options,
+    onChange: (instance) => {
+      requestAnimationFrame(() => {
+        virtualizer._willUpdate();
+        rerenderRef.value++;
+        options.onChange?.(instance);
+      });
+    },
+  });
+
+  const instance = computed(() => {
+    rerenderRef.value / 2;
+    virtualizer.setOptions({
+      ...options,
+      onChange: (instance) => {
+        requestAnimationFrame(() => {
+          virtualizer._willUpdate();
+          rerenderRef.value++;
+          options.onChange?.(instance);
+        });
+      },
+    });
+    return virtualizer;
+  });
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   return instance;
-
-  // the response of the virtualizer provides several values:
-  instance.scrollToIndex; //<-- fn to trigger a scroll.
-  instance.scrollElement; //<-- the element being scrolled, Readonly.
-  instance.getVirtualItems; // () => VirtualItems <-- you should render these virtual items.
-
-  /**
-   * In react: it looks like:
-   * {rowVirtualizer.getVirtualItems().map((virtualRow) => (
-            <div
-              key={virtualRow.index}
-            ref={virtualRow.measureElement} <-- this is pretty important, 
-                                            as it feeds back the height of the 
-                                            element to the renderer.
-              className={virtualRow.index % 2 ? 'ListItemOdd' : 'ListItemEven'}
-              style={{
-                position: 'absolute', top: 0, left: 0, width: '100%',
-                height: `${virtualRow.size}px`,
-                transform: `translateY(${virtualRow.start}px)`,
-              }}
-            >
-              Row {virtualRow.index}
-            </div>
-          ))}
-
-   */
-
-  instance.getTotalSize; // () => size of total space:
-
-  /**
-           * In React: use this to wrap the above getVirtualItems.
-           *       <div
-        ref={parentRef}
-        className="List"
-        style={{
-          height: `200px`,width: `400px`,overflow: 'auto',
-        }}
-      >
-        <div
-          style={{
-            height: rowVirtualizer.getTotalSize(), <-- Here we get total size.
-            width: '100%',position: 'relative',
-          }}
-        >
-            * {rowVirtualizer.getVirtualItems().map((virtualRow) => (
-                and so on and so on...
-           */
-
-  // in vue, the setup should be very similar, except the output should be reactive variables?
-  // How does that work? So... To summarize:
-
-  // for dynamic sizing, the row/item must report itself to a (Element) => void
-  // callback fn called 'measureElement(el)' after rendering.
-
-  // =================================================================
-  // TLDR & IDEAS:
-
-  // As the user scrolls, `observeElementRect(el, cb(rect) => void)`, and
-  // `observeElementOffset(el, cb(rect) => void)` will fire callbacks
-  // into the Virtualizer itself, which modifies the virtualizer.getVirtualItems(); output,
-  // which, if the UI is reactively looking at it, will cause a redraw.
-
-  // getVirtualItems should probably be a computed fn() which depends on
-  // observeRect and observeElementOffset.
-  //
-
-  // maybe if the offset changes..., it'll cause a sideeffect for getVirtualItems to fire?
-  // =============================
 }
 
 export function useVirtualizer<TScrollElement, TItemElement = unknown>(
@@ -94,7 +62,7 @@ export function useVirtualizer<TScrollElement, TItemElement = unknown>(
     VirtualizerOptions<TScrollElement, TItemElement>,
     "observeElementRect" | "observeElementOffset" | "scrollToFn"
   >
-): Virtualizer<TScrollElement, TItemElement> {
+): Ref<Virtualizer<TScrollElement, TItemElement>> {
   return useVirtualizerBase<TScrollElement, TItemElement>({
     observeElementRect: observeElementRect,
     observeElementOffset: observeElementOffset,
@@ -111,7 +79,7 @@ export function useWindowVirtualizer<TItemElement = unknown>(
     | "observeElementOffset"
     | "scrollToFn"
   >
-): Virtualizer<Window, TItemElement> {
+): Ref<Virtualizer<Window, TItemElement>> {
   return useVirtualizerBase<Window, TItemElement>({
     getScrollElement: () => (typeof window !== "undefined" ? window : null!),
     observeElementRect: observeWindowRect,
