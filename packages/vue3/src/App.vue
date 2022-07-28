e97cc525fc545fb9beee04f1009acd638a4b800d
David Chen <3145205+RiceCakess@users.noreply.github.com>
2022-07-27 20:14:21 -0700

    Clean up

--- packages/vue3/src/App.vue
+++ packages/vue3/src/App.vue
@@ -1,5 +1,26 @@
 <template>
-  <app-frame />
+  <v-app>
+    <main-nav />
+    <!-- <v-navigation-drawer app>
+      <template v-for="(menuItem, index) in menuList" :key="index">
+        <router-link :to="menuItem.link || '/'">
+          <v-btn>{{ menuItem.label }}</v-btn>
+        </router-link>
+      </template>
+
+    </v-navigation-drawer> -->
+
+    <!-- Sizes your content based upon application components -->
+    <v-main class="bg-base-300">
+      <!-- Provides the application the proper gutter -->
+      <!-- If using vue-router -->
+      <router-view />
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
 
 <script setup lang="ts">
