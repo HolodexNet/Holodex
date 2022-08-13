3859bddc2b9d2b0e961d254b5c1cf5f5375b85f9
David Chen <3145205+RiceCakess@users.noreply.github.com>
2022-08-12 22:11:33 -0700

    Rename to iCal feed

--- src/components/user/UserCard.vue
+++ src/components/user/UserCard.vue
@@ -86,7 +86,7 @@
         </v-list-item-icon>
         <v-list-item-content>
           <v-list-item-title>
-            Calendar Feed
+            iCal Feed
           </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
