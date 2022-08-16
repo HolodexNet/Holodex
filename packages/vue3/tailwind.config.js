16120f09739f8f4daa7b0b0e0e92cfe8825d6c39
David Chen <3145205+RiceCakess@users.noreply.github.com>
2022-08-16 01:38:20 -0700

    No prefix needed

--- packages/vue3/tailwind.config.js
+++ packages/vue3/tailwind.config.js
@@ -1,29 +1,25 @@
 
 const colors = { "primary": '--p', "secondary": '--s', "accent": '--a', "bgColor": '--b1', "neutral": "--n" };
 const shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
-// these are tailwind specific variables, check docs
-const prefixes = ["backgroundColor", "textColor", "borderColor", "outlineColor"];
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: Object.keys(colors).reduce((prev, key) => {
-      const cssVar = colors[key];
-      prefixes.forEach((prefix) => {
-        if (!prev[prefix]) prev[prefix] = {};
+    extend: {
+      colors: Object.keys(colors).reduce((prev, key) => {
+        const cssVar = colors[key];
         shades.forEach((shadeNum) => {
-          if (!prev[prefix][key]) prev[prefix][key] = {};
-          prev[prefix][key][shadeNum] = `hsl(var(${cssVar}-${shadeNum}) / <alpha-value>)`;
+          if (!prev[key]) prev[key] = {};
+          prev[key][shadeNum] = `hsl(var(${cssVar}-${shadeNum}) / <alpha-value>)`;
           // makes bg-primary same as bg-primary-400
           if (shadeNum === 400) {
-            prev[prefix][key]["DEFAULT"] = `hsl(var(${cssVar}) / <alpha-value>)`;
+            prev[key]["DEFAULT"] = `hsl(var(${cssVar}) / <alpha-value>)`;
           }
         });
-      });
-      return prev;
-    }, {})
-
+        return prev;
+      }, {})
+    }
   },
   plugins: [require("daisyui"), require("@tailwindcss/line-clamp")],
   daisyui: {
