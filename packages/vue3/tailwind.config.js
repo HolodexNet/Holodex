9790ca100ab38630f75a523273f4c8ef376324cd
sphinxrave <holofan123@gmail.com>
2022-08-17 01:21:07 -0700

    tailwind config to support outline color as well

--- packages/vue3/tailwind.config.js
+++ packages/vue3/tailwind.config.js
@@ -2,23 +2,25 @@
 const colors = { "primary": '--p', "secondary": '--s', "accent": '--a', "bgColor": '--b1', "neutral": "--n" };
 const shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
 
+const colorOpts = Object.keys(colors).reduce((prev, colorName) => {
+  const cssVar = colors[colorName];
+  shades.forEach((shadeNum) => {
+    if (!prev[colorName]) prev[colorName] = {};
+    prev[colorName][shadeNum] = `hsl(var(${cssVar}-${shadeNum}) / <alpha-value>)`;
+    // makes bg-primary same as bg-primary-400
+    if (shadeNum === 400) {
+      prev[colorName]["DEFAULT"] = `hsl(var(${cssVar}) / <alpha-value>)`;
+    }
+  });
+  return prev;
+}, {})
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundColor: Object.keys(colors).reduce((prev, colorName) => {
-        const cssVar = colors[colorName];
-        shades.forEach((shadeNum) => {
-          if (!prev[colorName]) prev[colorName] = {};
-          prev[colorName][shadeNum] = `hsl(var(${cssVar}-${shadeNum}) / <alpha-value>)`;
-          // makes bg-primary same as bg-primary-400
-          if (shadeNum === 400) {
-            prev[colorName]["DEFAULT"] = `hsl(var(${cssVar}) / <alpha-value>)`;
-          }
-        });
-        return prev;
-      }, {})
+      backgroundColor: colorOpts,
+      outlineColor: colorOpts,
     }
   },
   plugins: [require("daisyui"), require("@tailwindcss/line-clamp")],
