155f5dcfaee27fe4cdccd8d8b0d1bba7692976cd
NeloBlivion <41271523+NeloBlivion@users.noreply.github.com>
2022-08-30 16:14:15 +0100

    Create ChannelTweets.vue
    There is an existing `TwitterFeed.vue` used on the About page; this could be repurposed for this usage, but for now I opted to create a new file.

---
+++ src/views/channel_views/ChannelTweets.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container>
+    <a
+      class="twitter-timeline"
+      :data-height="$store.state.isMobile ? '410' : '100%'"
+      :data-theme="$vuetify.theme.dark ? 'dark' : 'light'"
+      :href="`https://twitter.com/${channelTwitter}`"
+    >
+      Loading Tweets from {{ channelName }}...
+    </a>
+  </v-container>
+</template>
+
+<script lang="ts">
+
+export default {
+    name: "ChannelTweets",
+    metaInfo() {
+        const vm = this;
+        return {
+            title: `${vm.channelName} - Tweets - Holodex`,
+        };
+    },
+    computed: {
+        channel() {
+            return this.$store.state.channel.channel;
+        },
+        channelName() {
+            const prop = this.$store.state.settings.nameProperty;
+            return this.channel[prop] || this.channel.name;
+        },
+        channelTwitter() {
+            return this.channel.twitter;
+        },
+    },
+    mounted() {
+        const tweetScript = document.createElement("script");
+        tweetScript.setAttribute("src", "https://platform.twitter.com/widgets.js");
+        document.head.appendChild(tweetScript);
+    },
+};
+</script>
