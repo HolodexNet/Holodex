8294d397c49d8bf53be4093601eec9192d34c263
David Chen <3145205+RiceCakess@users.noreply.github.com>
2022-11-19 17:33:48 -0800

    Auto logout on 401 error

--- packages/vue3/src/setup/setupQueryPlugin.ts
+++ packages/vue3/src/setup/setupQueryPlugin.ts
@@ -1,9 +1,12 @@
+import { useClient } from "@/hooks/auth/client";
 import type {
   QueryClientConfig,
   QueryObserverOptions,
 } from "@tanstack/query-core";
-import type { VueQueryPluginOptions } from "@tanstack/vue-query";
+import { QueryCache, VueQueryPluginOptions } from "@tanstack/vue-query";
 import { QueryClient } from "@tanstack/vue-query";
+import { AxiosError } from "axios";
+import { useToast } from "vue-toast-notification";
 import { broadcastQueryClient } from "./vue-query/broadcastQueryClient";
 import { createWebStoragePersister } from "./vue-query/createWebStoragePersister";
 import { persistQueryClient } from "./vue-query/persistQueryClient";
@@ -28,6 +31,22 @@ export const STATIC_QUERY_OPTIONS: QueryObserverOptions = {
 
 const queryClientConfig: QueryClientConfig = {
   defaultOptions: { queries: REGULAR_QUERY_OPTIONS },
+  queryCache: new QueryCache({
+    onError: async (error, query) => {
+      // Surprised vue context is working here :thinking:
+      const { logout } = useClient();
+      const { open: toast } = useToast();
+      // How to get status code fo error
+      if ((error as AxiosError).request.status === 401) {
+        logout();
+        toast({
+          position: "top-right",
+          message: "Authentication error",
+          type: "error",
+        });
+      }
+    },
+  }),
 };
 
 export const queryClient = new QueryClient(queryClientConfig);
