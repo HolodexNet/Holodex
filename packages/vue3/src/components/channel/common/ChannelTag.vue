6e3a0535c9feaebe0b147901f2fe1f2467759a2f
David Chen <3145205+RiceCakess@users.noreply.github.com>
2023-01-20 00:47:54 -0800

    Iterate

---
+++ packages/vue3/src/components/channel/common/ChannelTag.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="badge badge-lg pl-0 text-sm text-base-content bg-bgColor-300 border-bgColor-100"
+  >
+    <channel-img :channel="channel" rounded :size="24" class="pr-2" />
+    {{ channelName }}
+  </div>
+</template>
+<script lang="ts" setup>
+import { useLangStore } from "@/stores/lang";
+
+const props = defineProps<{ channel: ShortChannel }>();
+const langPrefs = useLangStore();
+const channelName = computed(
+  () =>
+    langPrefs.preferredLocaleFn(
+      props.channel.english_name,
+      props.channel.name
+    ) || ""
+);
+</script>
--- packages/vue3/src/views/WatchNew.vue
+++ packages/vue3/src/views/WatchNew.vue
@@ -1,49 +1,89 @@
 <template>
   <div v-if="id && video" class="flex">
     <div class="flex flex-1">
-      <div class="flex flex-col flex-1 pr-[300px]">
-        <watch-channel-bar :video="video" class="border-b border-bgColor-100" />
-        <div class="px-4 py-2">
+      <div class="flex flex-col flex-1 watch-left">
+        <!-- <watch-channel-bar :video="video" class="border-b border-bgColor-100" /> -->
+
+        <div class="p-2">
           <video-player
             ref="playerInstance"
             :video="video"
             class="watch-video rounded-xl overflow-hidden"
           ></video-player>
         </div>
-        <div class="py-1 px-2">
+
+        <div class="pb-1 px-2">
+          <!-- <div class="flex items-center"> -->
+          <!-- <div class="font-bold line-clamp-2">{{ video.title }}</div> -->
           <div class="flex items-center">
-            <div class="font-bold line-clamp-2">{{ video.title }}</div>
-            <div class="flex items-center ml-auto">
-              <div class="ml-auto flex">
-                <button class="btn btn-circle btn-sm btn-outline">
-                  <div class="i-tabler:layout-sidebar text-2xl" />
-                </button>
-                <button class="btn btn-circle btn-sm btn-outline">
-                  <save-to-playlist-btn :video="video" class="text-2xl" />
-                </button>
-                <button class="btn btn-circle btn-sm btn-outline">
-                  <div class="i-mdi:dots-vertical text-2xl" />
-                </button>
+            <div class="flex items-center">
+              <div v-if="video.topic_id" class="flex items-center text-sm">
+                <div class="i-mdi:tag mr-0.5"></div>
+                {{ video.topic_id }}
               </div>
+              &nbsp;
+              <div v-if="video.live_viewers" class="flex items-center text-sm">
+                <div class="i-mdi:account text-lg mr-0.5" />
+                {{ video.live_viewers }}
+              </div>
+              &nbsp;
+              <div class="i-mdi:clock text-lg mr-0.5" />
+              <video-card-live-duration
+                :video="video"
+                class="bg-transparent text-sm"
+              />
+            </div>
+            <div class="ml-auto flex">
+              <button class="btn btn-circle btn-sm btn-outline">
+                <div class="i-tabler:layout-sidebar text-2xl" />
+              </button>
+              <button class="btn btn-circle btn-sm btn-outline">
+                <save-to-playlist-btn :video="video" class="text-2xl" />
+              </button>
+              <button class="btn btn-circle btn-sm btn-outline">
+                <div class="i-mdi:dots-vertical text-2xl" />
+              </button>
             </div>
           </div>
           <div class="flex items-center">
-            <div v-if="video.topic_id" class="flex items-center text-sm">
-              <div class="i-mdi:tag mr-0.5"></div>
-              {{ video.topic_id }}
-            </div>
-            &nbsp;
-            <div v-if="video.live_viewers" class="flex items-center text-sm">
-              <div class="i-mdi:account text-lg mr-0.5" />
-              {{ video.live_viewers }}
-            </div>
-            &nbsp;
-            <div class="i-mdi:clock text-lg mr-0.5" />
-            <video-card-live-duration
-              :video="video"
-              class="bg-transparent text-sm"
+            <channel-img
+              :channel="video.channel"
+              :size="36"
+              rounded
+              class="mr-2 flex flex-shrink-0"
             />
+            <div>
+              <div class="font-bold line-clamp-2">{{ video.title }}</div>
+              <span class="opacity-80">{{ channelName }}</span>
+            </div>
+            <button class="btn btn-sm btn-outline btn-primary ml-auto">
+              <div class="i-mdi:heart text-xl" />
+            </button>
+            <button class="btn btn-sm btn-outline btn-primary">
+              <div class="i-mdi:share text-xl" />
+            </button>
           </div>
+          <div
+            v-if="video.mentions"
+            class="flex items-center flex-wrap mt-2 gap-y-2"
+          >
+            <div class="i-mdi:account-group text-2xl mr-1"></div>
+            <template
+              v-for="mention in mentionsShowMore
+                ? video.mentions
+                : video.mentions?.slice(0, 3)"
+            >
+              <channel-tag :channel="mention" class="mr-1" />
+            </template>
+            <a
+              v-if="video.mentions.length > 3"
+              class="text-primary cursor-pointer text-sm"
+              @click="mentionsShowMore = !mentionsShowMore"
+              >[{{ mentionsShowMore ? "-" : "+"
+              }}{{ video.mentions.length - 3 }}]</a
+            >
+          </div>
+          <!-- </div> -->
           <div class="divider m-0.5"></div>
           <h-truncated-text :text="video.description" class="px-2 opacity-90" />
         </div>
@@ -57,6 +97,7 @@
 <script lang="ts" setup>
 import { PlayerRef } from "@/components/player/usePlayer";
 import { useVideoById } from "@/services/video";
+import { useLangStore } from "@/stores/lang";
 const route = useRoute();
 const playerInstance = ref<PlayerRef | null>(null);
 const clipLangRef = computed(() => ({
@@ -69,10 +110,25 @@ const id = computed(() =>
 const { data: video, isLoading } = useVideoById(id, clipLangRef, {
   enabled: computed(() => !!id.value),
 });
+const langPrefs = useLangStore();
+
+const channelName = computed(
+  () =>
+    langPrefs.preferredLocaleFn(
+      video.value?.channel.english_name,
+      video.value?.channel.name
+    ) || ""
+);
+
+const mentionsShowMore = ref(false);
 watchEffect(() => console.log(playerInstance.value?.currentTime));
 </script>
 <style lang="scss">
 $nav-bar-height: 56px;
+
+.watch-left {
+  padding-right: 300px;
+}
 
 .watch-video {
   position: relative;
