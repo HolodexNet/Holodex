9a4752ddec0b92d19c1e6606d83a470adc0d2c06
David Chen <3145205+RiceCakess@users.noreply.github.com>
2023-01-28 23:49:00 -0800

    Fix chat message css

--- packages/vue3/src/components/tldex/client/ChatMessage.vue
+++ packages/vue3/src/components/tldex/client/ChatMessage.vue
@@ -1,16 +1,12 @@
 <template>
-  <div
-    class="flex flex-row"
-    :class="{ 'with-author': !hideAuthor && !source.shouldHideAuthor }"
-  >
+  <div class="flex flex-row" :class="{ 'with-author': !hideAuthor }">
     <div
       v-if="source.is_vtuber && source.channel_id"
       style="min-width: 28px"
       class="mr-2"
     >
       <channel-img
-        v-if="!hideAuthor && !source.shouldHideAuthor"
-        class="align-self-center"
+        v-if="!hideAuthor"
         :channel="{ id: source.channel_id, name: source.name }"
         :size="28"
         rounded
@@ -18,40 +14,28 @@
       />
     </div>
     <div style="flex-basis: 100%">
-      <div
-        v-if="!hideAuthor && !source.shouldHideAuthor"
-        :class="{
-          'tl-caption': true,
-          'text-primary': source.is_owner,
-          'text-secondary':
-            !source.is_owner &&
-            (source.is_verified || source.is_moderator || source.is_vtuber),
-        }"
-      >
-        <span class="tl-name" @click="showBlockChannelDialog = true">
-          <!-- <span v-if="source.is_owner">👑</span> -->
-          <span v-if="source.is_vtuber">[Vtuber]</span>
-          <span v-if="source.is_moderator">[Mod]</span>
-          <span v-if="source.source">{{ source.source }} - </span>
-          {{ source.name
-          }}<span v-if="source.is_verified" style="font-weight: 800"> ✓</span>:
-          <v-icon
-            x-small
-            style="margin-top: 2px; position: absolute; width: 11px"
-            >{{ icons.mdiCog }}</v-icon
-          >
+      <div v-if="!hideAuthor" :class="nameClass">
+        <span class="tl-name tl-caption" @click="showBlockChannelDialog = true">
+          <span v-if="source.is_vtuber">[Vt]</span>
+          <div
+            v-if="source.is_moderator"
+            class="i-mdi:wrench font-sm inline-block"
+          ></div>
+          {{ source.name }}
+          <span v-if="source.is_verified" style="font-weight: 800"> ✓</span>:
+          <div
+            style="margin-top: 3px; position: absolute"
+            class="i-mdi:cog font-sm tl-cog inline-block"
+          ></div>
         </span>
       </div>
       <a class="tl-message" :data-time="source.relativeMs / 1000">
-        <span v-if="source.timestamp" class="tl-caption mr-1">
+        <span v-if="source.timestamp" class="tl-caption mr-1 opacity-75">
           {{ time }}
         </span>
-        <span
-          v-if="source.parsed"
-          class="text-primary"
-          v-html="source.parsed"
-        />
-        <span v-else class="text-primary">{{ source.message }}</span>
+        <!-- eslint-disable-next-line vue/no-v-html !-->
+        <span v-if="source.parsed" v-html="source.parsed" />
+        <span v-else>{{ source.message }}</span>
       </a>
     </div>
     <h-dialog v-model="showBlockChannelDialog">
@@ -130,6 +114,17 @@ export default defineComponent({
         : (Math.sign(this.source.relativeMs) < 0 ? "-" : "") +
             formatDuration(Math.abs(this.source.relativeMs));
     },
+    nameClass() {
+      const { is_owner, is_verified, is_vtuber, is_moderator } = this.source;
+      switch (true) {
+        case is_owner:
+          return "text-primary";
+        case is_verified || is_moderator || is_vtuber:
+          return "text-secondary";
+        default:
+          return "opacity-75";
+      }
+    },
   },
   methods: {
     toggleBlockName(name: string) {
@@ -140,18 +135,6 @@ export default defineComponent({
 </script>
 
 <style>
-.theme--light .tl-caption {
-  color: hsla(0, 0%, 30%, 0.7);
-}
-.theme--dark .tl-caption {
-  color: hsla(0, 0%, 70%, 0.7);
-}
-
-.tl-body .tl-caption {
-  letter-spacing: 0.0333333333em !important;
-  font-size: 0.85em;
-}
-
 .tl-name,
 .tl-message {
   word-break: break-word;
@@ -160,11 +143,11 @@ export default defineComponent({
 .tl-name {
   cursor: pointer;
 }
-.tl-name .v-icon {
+.tl-name .tl-cog {
   opacity: 0;
 }
 
-.tl-name:hover .v-icon {
+.tl-name:hover .tl-cog {
   opacity: 1;
 }
 
@@ -179,5 +162,10 @@ export default defineComponent({
   width: auto;
   height: 1.3em;
   vertical-align: middle;
+}
+
+.tl-caption {
+  letter-spacing: 0.0333333333em !important;
+  font-size: 0.85em;
 }
 </style>
