386ca20f847e6357674f52d66132088177e50f4a
David Chen <3145205+RiceCakess@users.noreply.github.com>
2023-02-12 03:13:39 -0800

    fix mobile value

--- packages/vue3/src/hooks/common/useDisplay.ts
+++ packages/vue3/src/hooks/common/useDisplay.ts
@@ -2,7 +2,7 @@ import { breakpointsTailwind, useBreakpoints } from "@vueuse/core";
 
 export function useDisplay() {
   const breakpoints = useBreakpoints(breakpointsTailwind);
-  const mobile = computed(() => breakpoints.sm);
+  const mobile = computed(() => breakpoints.isSmallerOrEqual("sm"));
 
   return { mobile, ...breakpoints };
 }
