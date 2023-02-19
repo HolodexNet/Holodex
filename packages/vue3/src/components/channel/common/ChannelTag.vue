2aaf7b90aae02eb40b3380b30209b5d53a4bcc8b
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2023-02-18 21:25:15 -0800

    allow custom color on ChannelTag

--- packages/vue3/src/components/channel/common/ChannelTag.vue
+++ packages/vue3/src/components/channel/common/ChannelTag.vue
@@ -1,8 +1,10 @@
 <template>
   <div
-    class="badge badge-lg border-bgColor-100 bg-bgColor-300 text-sm text-base-content"
+    class="badge badge-lg border-bgColor-100 text-sm text-base-content"
     :class="{
       'pl-0': !tile,
+      'bg-bgColor-300': !color,
+      [color!]: color,
     }"
   >
     <channel-img :channel="channel" :rounded="!tile" :size="24" class="mr-2" />
@@ -12,7 +14,11 @@
 <script lang="ts" setup>
 import { useLangStore } from "@/stores/lang";
 
-const props = defineProps<{ channel: ShortChannel; tile?: boolean }>();
+const props = defineProps<{
+  channel: ShortChannel;
+  tile?: boolean;
+  color: string;
+}>();
 const langPrefs = useLangStore();
 const channelName = computed(
   () =>
