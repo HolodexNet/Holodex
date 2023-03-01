e795eb69eaea8a13386132ffd1e6b7c9a4528089
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2023-03-01 00:47:42 -0800

    video card template change

--- packages/vue3/src/components/video/card/VideoCardTemplate.vue
+++ packages/vue3/src/components/video/card/VideoCardTemplate.vue
@@ -1,12 +1,12 @@
 <template>
   <div role="listitem" class="flex">
-    <div v-if="$slots.avatar" class="mx-2 self-center">
-      <slot name="avatar"/>
+    <div v-if="$slots.avatar" class="mr-2 self-center">
+      <slot name="avatar" />
     </div>
     <div class="flex flex-1 flex-col">
-      <slot/>
+      <slot />
     </div>
-    <slot name="action"/>
+    <slot name="action" />
   </div>
 </template>
 <script lang="ts" setup></script>
