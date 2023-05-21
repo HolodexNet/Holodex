677a54be3602ff4157f9da9a7d91202384640f55
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2023-05-20 21:36:15 -0700

    polish about page

--- packages/vue3/src/views/about/AboutGeneral.vue
+++ packages/vue3/src/views/about/AboutGeneral.vue
@@ -5,7 +5,9 @@
       <b>Holodex</b>
       is a fan-built platform for watching VTubers. We are built by DD for DDs.
     </p>
-    <holodex-stats />
+    <h-row class="items-center">
+      <holodex-stats class="my-2 grow justify-center" />
+    </h-row>
     <p>
       We organize clips and collabs by talents included, tabulate songs sang
       inside individual videos and as part of karaoke streams, compile agencies
@@ -29,17 +31,19 @@
       We'd like to thank our long list of contributors and over 100 editors:
     </p>
     <ul class="list-item list-inside indent-4">
-      <li>RiceCakes (creator, admin)</li>
-      <li>Xrave (admin)</li>
-      <li>Uetchy (dev, designer)</li>
-      <li>MadeT (designer)</li>
-      <li>NeloBlivion (twitch support, editor)</li>
-      <li>...</li>
-      <!-- TBD -->
+      <li class="list-disc">RiceCakes (creator, admin)</li>
+      <li class="list-disc">Xrave (admin)</li>
+      <li class="list-disc">Uetchy (dev, designer)</li>
+      <li class="list-disc">MadeT (designer)</li>
+      <li class="list-disc">NeloBlivion (twitch support, editor)</li>
+      <li class="list-disc opacity-60">
+        <small>
+          (If you'd like to add your name to this list, drop us a poke on
+          Discord!)
+        </small>
+      </li>
     </ul>
-    <p class="text-sm opacity-60">
-      (If you'd like to add your name to this list, drop us a poke on Discord!)
-    </p>
+    <p class="text-sm opacity-60" />
     <h3 class="mb-2 mt-4 text-lg">Quick Links</h3>
     <div
       id="qlinks"
@@ -60,15 +64,24 @@
       >
         <div class="i-cib:ko-fi" />
       </a>
-      <a class="btn-outline btn-primary btn p-0" title="Issues / Source Code">
+      <a
+        class="btn-outline btn-primary btn p-0"
+        title="Issues / Source Code"
+        href="https://github.com/HolodexNet/Holodex"
+      >
         <div class="i-carbon:logo-github" />
       </a>
-      <a class="btn-outline btn-warning btn p-0" title="API Documentation">
+      <a
+        class="btn-outline btn-warning btn p-0"
+        title="API Documentation"
+        href="https://docs.holodex.net/docs/holodex/f4e6fa31af431-getting-started"
+      >
         <div class="i-fluent:document-flowchart-20-regular" />
       </a>
       <a
         class="btn-outline btn border-purple-500 p-0 text-purple-500 hover:bg-purple-500"
         title="Holodex Discord"
+        href="https://discord.gg/A24AbzgvRJ"
       >
         <div class="i-carbon:logo-discord" />
       </a>
