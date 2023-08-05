e241198af761ddb259969c415cfebf2fa67df6a1
UK <41271523+NeloBlivion@users.noreply.github.com>
2023-08-05 17:30:18 +0100

    oops

--- src/components/watch/WatchSubtitleOverlay.vue
+++ src/components/watch/WatchSubtitleOverlay.vue
@@ -46,7 +46,7 @@ export default {
     box-shadow: 10px 0 0 rgba(0,0,0,0.75), -10px 0 0 rgba(0,0,0,0.75);
     background: rgba(0,0,0,0.75);
     white-space: pre-wrap;
-    color: rgba(255,255,255,255,1);
+    color: rgba(255,255,255,1)
 }
 .subtitle-name {
   font-size: 16px;
