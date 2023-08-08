04e9d30a0328409ee6a8f3e935c0340175a65286
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2023-08-07 20:14:40 -0700

    switching kitchensink out for a example Frame

--- packages/react/src/main.tsx
+++ packages/react/src/main.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
+import { Frame } from "./components/layout/Frame.tsx";
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Theme accentColor="amber" appearance="dark">
-      <App />
+      <Frame />
     </Theme>
   </React.StrictMode>,
 );
