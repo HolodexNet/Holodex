726228ca80b3f85e7f130bcc5cf7bab1188d19a6
Justin <justintay123@gmail.com>
2023-09-17 17:54:04 +0800

    views: HomeFave: replace maps containing "list" with "collabs

--- src/views/HomeFave.vue
+++ src/views/HomeFave.vue
@@ -43,6 +43,9 @@
         <v-tab class="pa-2">
           {{ $t("views.home.recentVideoToggles.subber") }}
         </v-tab>
+        <v-tab class="pa-2">
+          {{ $t("views.home.recentVideoToggles.collabs") }}
+        </v-tab>
         <portal-target v-if="!$vuetify.breakpoint.xs" :name="`date-selector${isFavPage}`" class=" v-tab ml-auto" />
       </v-tabs>
     </portal>
@@ -105,7 +108,7 @@ export default {
                 LIVE_UPCOMING: 0,
                 ARCHIVE: 1,
                 CLIPS: 2,
-                LIST: 3,
+                COLLABS: 3,
             }),
             refreshTimer: null,
         };
@@ -209,7 +212,7 @@ export default {
                 0: "",
                 1: "archive",
                 2: "clips",
-                3: "list",
+                3: "collabs",
             };
             this.$router
                 .replace({
@@ -231,6 +234,9 @@ export default {
                     break;
                 case "#clips":
                     this.tab = this.Tabs.CLIPS;
+                    break;
+                case "#collabs":
+                    this.tab = this.Tabs.COLLABS;
                     break;
                 case "#list":
                     this.tab = this.Tabs.LIST;
