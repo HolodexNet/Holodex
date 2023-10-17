f3a5f90932d4b00226105f09266721443d8b6e3d
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2023-10-16 21:25:19 -0700

    break variants out of component exports for fast-refresh

--- packages/react/src/shadcn/ui/dialog.tsx
+++ packages/react/src/shadcn/ui/dialog.tsx
@@ -3,8 +3,9 @@ import * as DialogPrimitive from "@radix-ui/react-dialog"
 import { Cross2Icon } from "@radix-ui/react-icons"
 
 import { cn } from "@/lib/utils"
-import { type VariantProps, cva } from "class-variance-authority"
+import { type VariantProps } from "class-variance-authority"
 import { badgeVariants } from "./badge"
+import { dialogVariants } from "./dialog.variants"
 
 const Dialog = DialogPrimitive.Root
 
@@ -26,19 +27,6 @@ const DialogOverlay = React.forwardRef<
   />
 ))
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
-
-const dialogVariants = cva(
-  "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full",
-  {
-    variants: {
-      variant: {
-        default: "border-base-7 bg-base-1",
-        primary: "border-primary-7 bg-primary-1",
-        secondary: "border-secondary-7 bg-secondary-1",
-      },
-    },
-  }
-);
 
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
@@ -124,7 +112,6 @@ export {
   DialogPortal,
   DialogOverlay,
   DialogTrigger,
-  dialogVariants,
   DialogContent,
   DialogHeader,
   DialogFooter,
