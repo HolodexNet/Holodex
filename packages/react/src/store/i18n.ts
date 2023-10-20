188bd831b95923ed014a84c3cc61d29f411085e1
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-10-20 19:08:32 -0400

    created a language atom

--- packages/react/src/store/i18n.ts
+++ packages/react/src/store/i18n.ts
@@ -1,7 +1,14 @@
 import dayjs from "dayjs";
 import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 
 export const localeAtom = atom({
   lang: window.localStorage.getItem("i18nextLng") ?? navigator.language,
   dayjs: (...args: Parameters<typeof dayjs>) => dayjs(...args),
+});
+
+export const currentLangAtom = atomWithStorage<Lang>("lang", {
+  val: "en",
+  display: "English",
+  short: "en",
 });
