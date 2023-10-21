cf5ff1a21ef8f1386b53c3cd746f082dd0499598
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2023-10-21 10:39:23 -0700

    upgrade package

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -15,15 +15,53 @@ export default defineConfig({
     // sourcemap: true
   },
   server: {
-    port: 3000,
+    port: 8080,
     proxy: {
       "/api": {
-        target: "https://staging.holodex.net/",
+        target: "https://staging.holodex.net",
         changeOrigin: true,
+        headers: { origin: "https://staging.holodex.net" },
+        secure: false,
+        ws: true,
+        configure: (proxy, _options) => {
+          proxy.on("error", (err, _req, _res) => {
+            console.log("proxy error", err);
+          });
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log(
+              "Sending Request:\n\t",
+              req.method,
+              req.url,
+              "\n\t => TO THE TARGET => \n\t",
+              proxyReq.method,
+              proxyReq.protocol,
+              proxyReq.host,
+              proxyReq.path,
+              "\n",
+              JSON.stringify(proxyReq.getHeaders(), null, 2),
+            );
+          });
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
+            console.log(
+              "Received Response from the Target:",
+              proxyRes.statusCode,
+              req.url,
+              JSON.stringify(proxyRes.headers),
+            );
+          });
+        },
+      },
+      "^/(stats|orgs).json$": {
+        target: "https://staging.holodex.net",
+        changeOrigin: true,
+        headers: { origin: "https://staging.holodex.net" },
+        secure: false,
       },
       "/statics": {
-        target: "https://staging.holodex.net/",
+        target: "https://staging.holodex.net",
         changeOrigin: true,
+        headers: { origin: "https://staging.holodex.net" },
+        secure: false,
       },
     },
   },
