ead13341d98fde8e14f47ee42cf84684818cbd7b
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-10-25 14:34:41 -0400

    removed favourites store

--- packages/react/src/store/favourites.ts
+++
@@ -1,7 +0,0 @@
-import { atom, useSetAtom } from "jotai";
-import { atomWithStorage } from "jotai/utils";
-
-export const favouritesAtom = atomWithStorage<FavoriteChannel | null>(
-  "favourites",
-  null,
-);
