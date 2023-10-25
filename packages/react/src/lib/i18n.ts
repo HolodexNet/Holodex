0dae93365159edce85330eb26b5cfe3785951490
P-man2976 <ikmy1119924@gmail.com>
2023-10-26 01:09:07 +0900

    Remove unused language property

--- packages/react/src/lib/i18n.ts
+++ packages/react/src/lib/i18n.ts
@@ -11,127 +11,107 @@ import { loadDayJsLocale } from "./i18nUtils";
 
 const store = getDefaultStore();
 export const langs = [
-  { val: "en", locale: "en", display: "English", credit: "@Holodex" },
+  { val: "en", display: "English", credit: "@Holodex" },
   {
     val: "en-CA",
-    locale: "en",
     display: "English (Canadian)",
     credit: "@Holodex",
   },
   {
     val: "en-GB",
-    locale: "en",
     display: "English (British)",
     credit: "@Holodex",
   },
   {
     val: "lol-UWU",
-    locale: "en",
     display: "English (UwU)",
     credit: "Doubleturtle#3660",
   },
   {
     val: "lol-PEKO",
-    locale: "en",
     display: "English (PEKO)",
     credit: "Doubleturtle#3660",
   },
   {
     val: "ja-JP",
-    locale: "ja-JP",
     display: "日本語",
     credit: "Yourein#3960,Saginomiya#2353",
   },
   {
     val: "zh-TW",
-    locale: "zh-TW",
     display: "繁體中文",
     credit: "angel84326#7887",
   },
-  { val: "zh-CN", locale: "zh-CN", display: "简体中文", credit: "ttg#6038" },
+  { val: "zh-CN", display: "简体中文", credit: "ttg#6038" },
   {
     val: "ko-KR",
-    locale: "ko-KR",
     display: "한국어",
     credit: "AlexKoala#0253",
   },
   {
     val: "es-ES",
-    locale: "es-ES",
     display: "Español España",
     credit: "TraduSquare (Darkc0m y D3fau4)",
   },
   {
     val: "es-MX",
-    locale: "es-MX",
     display: "Español Latino",
     credit: "Aldo#3682",
   },
   {
     val: "ms-MY",
-    locale: "ms-MY",
     display: "Bahasa Melayu",
     credit: "Admiy#8261",
   },
   {
     val: "id-ID",
-    locale: "id-ID",
     display: "Bahasa Indonesia",
     credit: "alcyneous#2803",
   },
   {
     val: "ru-RU",
-    locale: "ru-RU",
     display: "Русский язык",
     credit: "kirillbarnaul#8499",
   },
   {
     val: "pt-BR",
-    locale: "pt-BR",
     display: "Português Brasileiro",
     credit: "Ash Niartis#5090",
   },
   {
     val: "de-DE",
-    locale: "de-DE",
     display: "Deutsch",
     credit: "DatJocab#1803, Doubleturtle#3660",
   },
   {
     val: "it-IT",
-    locale: "it-IT",
     display: "Italiano",
     credit: "テオさん#0139",
   },
   {
     val: "fr-FR",
-    locale: "fr-FR",
     display: "Français",
     credit: "pinembour#7770,Derasiel △#0002",
   },
   {
     val: "tr-TR",
-    locale: "tr-TR",
     display: "Türkçe",
     credit: "creeperkafasipw#1861",
   },
   {
     val: "vi-VN",
-    locale: "vi-VN",
     display: "Tiếng Việt",
     credit: "Pooh#6666,Dead xda member#4848,#Hiraoka Yukio#3042",
   },
   {
     val: "hu-HU",
-    locale: "hu-HU",
     display: "Magyar",
     credit: "kuroihikikomori#7216",
   },
-  { val: "th-TH", locale: "th-TH", display: "ไทย", credit: "SnowNeko#0282" },
+  { val: "th-TH", display: "ไทย", credit: "SnowNeko#0282" },
 ] as const;
 
 export type SupportedLangCodes = (typeof langs)[number]["val"];
-export type LocaleCodes = (typeof langs)[number]["locale"];
 
 i18n
   .use(ChainedBackend)
@@ -154,11 +134,7 @@ i18n
     backend: {
       backends: [
         resourcesToBackend(
-          (language: string) =>
-            import(
-              `@/locales/${langs.find(({ val }) => val === language)
-                ?.locale}/ui.yml`
-            ),
+          (language: string) => import(`@/locales/${language}/ui.yml`),
         ),
       ],
     },
