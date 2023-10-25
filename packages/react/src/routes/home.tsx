fe18a1fbe46a1eedd845699cedffb1af1cd5c33c
P-man2976 <ikmy1119924@gmail.com>
2023-10-26 01:09:56 +0900

    replace history at home searchparams

--- packages/react/src/routes/home.tsx
+++ packages/react/src/routes/home.tsx
@@ -89,7 +89,7 @@ export function Home() {
   useEffect(() => {
     console.log(`tab changed ${tab}`);
     searchParams.set("tab", tab);
-    setSearchParams(searchParams);
+    setSearchParams(searchParams, { replace: true });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchParams, tab]);
 
@@ -101,18 +101,18 @@ export function Home() {
       defaultValue={tab}
       onValueChange={setTab}
     >
-      <TabsList className="z-20 flex w-full justify-start overflow-x-auto overflow-y-hidden rounded-none bg-base-2">
+      <TabsList className="bg-base-2 z-20 flex w-full justify-start overflow-x-auto overflow-y-hidden rounded-none">
         <TabsTrigger className="text-lg" value="live">
           <Trans
             i18nKey="views.home.liveOrUpcomingHeading"
             components={{
               liveCount: (
-                <span className="mx-1 rounded-sm bg-secondary-5 p-1 text-sm">
+                <span className="bg-secondary-5 mx-1 rounded-sm p-1 text-sm">
                   {live?.filter(({ status }) => status === "live").length}
                 </span>
               ),
               upcomingCount: (
-                <span className="ml-1 rounded-sm bg-secondary-5 p-1 text-sm">
+                <span className="bg-secondary-5 ml-1 rounded-sm p-1 text-sm">
                   {live?.filter(({ status }) => status === "upcoming").length}
                 </span>
               ),
