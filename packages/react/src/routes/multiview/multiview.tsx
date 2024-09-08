8a63da87ea71f351d5245849378bc419ea230c38
Vincent <87549152+trash-lobster@users.noreply.github.com>
2024-09-09 08:22:47 +1200

    Create multiview placeholder (#1)

---
+++ packages/react/src/routes/multiview/multiview.tsx
@@ -0,0 +1,16 @@
+import { Helmet } from "react-helmet-async";
+
+// multiview skeleton
+// selection bar at the top to change between orgs and allow url insertion
+// grid page for drag and drop
+
+export function Multiview() {
+  return (
+    <>
+      <Helmet>
+        <title>Multiview - Holodex</title>
+      </Helmet>
+      <div>Placeholder</div>
+    </>
+  );
+}
