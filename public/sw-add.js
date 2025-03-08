bad353a2cc311d7c56c62878d1c7df0eb3136650
David Chen <3145205+RiceCakess@users.noreply.github.com>
2025-03-08 01:48:52 -0800

    Actually force updates

--- public/sw-add.js
+++ public/sw-add.js
@@ -1,5 +1,16 @@
+// Temporary add on code to sw.js to force updates
+
 /* eslint-disable no-restricted-globals */
 self.addEventListener("install", (event) => {
-    console.log("Service Worker installing.", event);
+    console.log("[New worker] Service Worker installing.", event);
+    self.skipWaiting();
     //   updateServiceWorkerFn();
+});
+
+/* eslint-disable no-restricted-globals */
+self.addEventListener("activate", (event) => {
+    console.log("[New worker] Service Worker activating.");
+
+    // Take control of all clients/tabs immediately
+    event.waitUntil(self.clients.claim());
 });
