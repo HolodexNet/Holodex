ab4efa041f05ea4eb759bcb158129d17b85adc15
Andrew Wong <ndrew.wong@gmail.com>
2025-03-15 19:03:03 -0500

    Allow checkbox rings to be seen when focused with a keyboard

--- packages/react/src/shadcn/ui/checkbox.tsx
+++ packages/react/src/shadcn/ui/checkbox.tsx
@@ -12,7 +12,7 @@ const Checkbox = React.forwardRef<
     ref={ref}
     className={cn(
       `peer h-4 w-4 shrink-0 rounded-sm border border-base-10 shadow 
-      focus-visible:ring-primaryA-5 focus-visible:ring-offset-base-2 focus-visible:outline-none 
+      focus-visible:ring-primaryA-5 focus-visible:ring-offset-base-2 
       disabled:cursor-not-allowed disabled:opacity-50 
       data-[state=checked]:bg-primary-9 data-[state=checked]:text-primary-12 data-[state=checked]:border-base-12 
       data-[state=checked]:ring-2`,
