df1131def1c881732c1b11f0b7c5fc9bea1f99fe
Vincent Sit <vincentsit00@ymail.com>
2025-07-11 17:15:00 +1200

    Add multiview bar toggle function

--- packages/react/src/routes/multiview/multiview.tsx
+++ packages/react/src/routes/multiview/multiview.tsx
@@ -1,11 +1,15 @@
 import { Selector } from "@/components/multiview/Selector";
+import { ToolButton } from "@/components/multiview/ToolButton";
 import { ToolButtonContainer } from "@/components/multiview/ToolButtonContainer";
 import {
   isSidebarOpenAtom,
+  multiViewPanelOpenAtom,
+  openMultiViewPanelAtom,
   sidebarShouldBeFullscreenAtom,
 } from "@/hooks/useFrame";
 import { cn } from "@/lib/utils";
-import { useAtom } from "jotai";
+import { mdiChevronDown } from "@mdi/js";
+import { useAtom, useSetAtom } from "jotai";
 import { Helmet } from "react-helmet-async";
 import { useTranslation } from "react-i18next";
 
@@ -19,7 +23,9 @@ export function Multiview() {
       <Helmet>
         <title>Multiview - Holodex</title>
       </Helmet>
-      <StickyBar />
+      <div className="relative">
+        <StickyBar />
+      </div>
     </>
   );
 }
@@ -28,23 +34,41 @@ function StickyBar() {
   const { t } = useTranslation();
   const [open] = useAtom(isSidebarOpenAtom);
   const [isFullScreen] = useAtom(sidebarShouldBeFullscreenAtom);
+  const [isBarActive] = useAtom(multiViewPanelOpenAtom);
+  const openPanel = useSetAtom(openMultiViewPanelAtom);
 
   return (
-    <div
-      className={cn(
-        "top-0 z-20 flex items-stretch justify-start overflow-x-auto rounded-none bg-base-2 p-2 transition-all md:px-10",
-        //isStuckAtTop && "rounded-lg md:mx-8 md:px-2",
-        !open ? "sticky" : isFullScreen ? "" : "sticky",
-      )}
-    >
-      <div className="flex w-full max-w-full flex-nowrap justify-between bg-base-2">
-        <div className="mr-2 w-3/5 shrink-0 grow-0 basis-auto">
-          <Selector />
-        </div>
-        <div className="ml-2 w-2/5 shrink-0 grow-0 basis-auto">
-          <ToolButtonContainer />
+    <>
+      <div
+        className={cn(
+          "top-0 z-20 flex max-w-full items-stretch justify-start overflow-x-auto rounded-none bg-base-2 p-2 transition-all md:px-10",
+          //isStuckAtTop && "rounded-lg md:mx-8 md:px-2",
+          !open ? "sticky" : isFullScreen ? "" : "sticky",
+          isBarActive ? "visible" : "hidden",
+        )}
+      >
+        <div className="flex w-full max-w-full flex-nowrap justify-between bg-base-2">
+          <div className="mr-2 w-3/5 shrink-0 grow-0 basis-auto">
+            <Selector />
+          </div>
+          <div className="ml-2 flex h-full w-2/5 shrink-0 grow-0 basis-auto items-center justify-end">
+            <ToolButtonContainer />
+          </div>
         </div>
       </div>
-    </div>
+      <ToolButton
+        className={cn(
+          "right-2 top-0 z-20 rounded-none bg-base-2 p-1 transition-all md:px-5",
+          // !open ? "sticky" : isFullScreen ? "" : "sticky",
+          "absolute",
+          isBarActive ? "hidden" : "visible",
+        )}
+        icon={{
+          path: mdiChevronDown,
+          tooltip: "Open Panel",
+          onClick: openPanel,
+        }}
+      />
+    </>
   );
 }
