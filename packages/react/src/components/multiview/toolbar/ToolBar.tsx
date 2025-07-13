4566804a4d16be2993ee5aca36d9fee91467888c
Vincent Sit <vincentsit00@ymail.com>
2025-07-13 16:20:25 +1200

    Fix merges issues

--- packages/react/src/components/multiview/toolbar/ToolBar.tsx
+++ packages/react/src/components/multiview/toolbar/ToolBar.tsx
@@ -1,15 +1,12 @@
-/* eslint-disable tailwindcss/no-custom-classname */
 import { cn } from "@/lib/utils";
-import { mdiChevronDown } from "@mdi/js";
-import { ToolButton } from "./ToolButton";
 import { ToolButtonContainer } from "./ToolButtonContainer";
 import {
+  isMobileAtom,
   isSidebarOpenAtom,
   multiViewPanelOpenAtom,
-  openMultiViewPanelAtom,
   sidebarShouldBeFullscreenAtom,
 } from "@/hooks/useFrame";
-import { useAtom, useSetAtom } from "jotai";
+import { useAtom, useAtomValue } from "jotai";
 // import { useTranslation } from "react-i18next";
 import { defaultOrgs } from "@/store/org";
 import {
@@ -23,13 +20,18 @@ import { useLive } from "@/services/live.service";
 import { useRef, useState } from "react";
 import { useVideoFilter } from "@/hooks/useVideoFilter";
 import { useVideoSort } from "@/hooks/useVideoSort";
+import { MultiViewIcon } from "./ToolButton";
 
-export function ToolBar() {
+type VideoWithExtra = VideoBase & {
+  platform: string;
+};
+
+export function ToolBar({ icons }: { icons: MultiViewIcon[] }) {
   //   const { t } = useTranslation();
   const [open] = useAtom(isSidebarOpenAtom);
   const [isFullScreen] = useAtom(sidebarShouldBeFullscreenAtom);
   const [isBarActive] = useAtom(multiViewPanelOpenAtom);
-  const openPanel = useSetAtom(openMultiViewPanelAtom);
+  const isMobile = useAtomValue(isMobileAtom);
 
   // create a mock favourites object as an org
   const Favorites: Org = {
@@ -37,12 +39,11 @@ export function ToolBar() {
   };
   // based on what the selection is -> use different methods to render title card?
   const [currentOrg, setCurrentOrg] = useState(Favorites);
-  const [liveChannels, setLiveChannels] = useState<Live[]>([]);
   const { data: live } = useLive({
     org: currentOrg.name,
-    type: ["placeholder", "stream"],
-    include: ["mentions"],
+    type: ["stream"],
   });
+
   const liveChannelContainerRef = useRef<HTMLDivElement>(null);
 
   const liveFiltered = useVideoFilter(
@@ -53,6 +54,12 @@ export function ToolBar() {
 
   // sort livestreams by video list settings
   const nowLiveSorted = useVideoSort(liveFiltered, "stream_schedule");
+  const nowLiveSortedWithPlatform: VideoWithExtra[] = nowLiveSorted.map(
+    (video) => ({
+      ...video,
+      platform: (video as VideoWithExtra).platform ?? "",
+    }),
+  );
 
   const onSelect = (org: Org) => {
     if (org.name === currentOrg.name) return;
@@ -69,57 +76,48 @@ export function ToolBar() {
   };
 
   return (
-    <>
-      <div
-        className={cn(
-          "top-0 grid max-w-full grid-cols-[auto,1fr,auto] items-stretch gap-2 rounded-none bg-base-2 p-2 transition-all md:px-10",
-          //isStuckAtTop && "rounded-lg md:mx-8 md:px-2",
-          !open ? "sticky" : isFullScreen ? "" : "sticky",
-          isBarActive ? "visible" : "hidden",
-        )}
-      >
-        <DropdownMenu>
-          <DropdownMenuTrigger className="flex min-h-8 w-48 shrink-0 items-center justify-between overflow-hidden rounded-md bg-base-2 pl-4 pr-2 hover:bg-primary-5 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary-7 active:scale-[97%] active:bg-primaryA-7 disabled:pointer-events-none disabled:opacity-50">
-            {currentOrg.name}
-            <div className="i-lucide:chevrons-down ml-2 inline-block h-4 w-4 shrink-0 align-middle opacity-50"></div>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="z-30 w-48 bg-base-2">
-            {[Favorites, ...defaultOrgs].map((org) => {
-              return (
-                <DropdownMenuItem
-                  className="cursor-pointer gap-1 px-4 py-2 hover:bg-primary-5"
-                  onClick={() => onSelect(org)}
-                >
-                  {org.name}
-                </DropdownMenuItem>
-              );
+    <div
+      id="multiview-toolbar"
+      className={cn(
+        "top-0 z-20 grid h-[var(--toolbar-height)] max-w-full items-stretch gap-2 rounded-none bg-base-2 p-2 transition-all duration-300 md:px-10",
+        !open ? "sticky" : isFullScreen ? "" : "sticky",
+        isBarActive ? "visible" : "hidden",
+        isMobile ? "justify-center" : "grid-cols-[auto_1fr_auto]",
+      )}
+    >
+      {!isMobile && (
+        <>
+          <DropdownMenu>
+            <DropdownMenuTrigger className="flex min-h-8 w-48 shrink-0 items-center justify-between overflow-hidden rounded-md bg-base-2 pl-4 pr-2 hover:bg-primary-5 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary-7 active:scale-[97%] active:bg-primaryA-7 disabled:pointer-events-none disabled:opacity-50">
+              {currentOrg.name}
+              <div className="shrink-0 ml-2 inline-block h-4 w-4 align-middle opacity-50 i-lucide:chevrons-down"></div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-48 bg-base-2 z-30">
+              {[Favorites, ...defaultOrgs].map((org) => {
+                return (
+                  <DropdownMenuItem
+                    className="hover:bg-primary-5 cursor-pointer gap-1 px-4 py-2"
+                    onClick={() => onSelect(org)}
+                  >
+                    {org.name}
+                  </DropdownMenuItem>
+                );
+              })}
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <div
+            id="live-channel-container"
+            ref={liveChannelContainerRef}
+            onWheel={handleWheel}
+            className={cn("flex min-h-12 w-full gap-2 overflow-x-scroll", {})}
+          >
+            {nowLiveSortedWithPlatform.map((live) => {
+              return <LiveChannel key={live.id} video={live} />;
             })}
-          </DropdownMenuContent>
-        </DropdownMenu>
-        <div
-          id="live-channel-container"
-          ref={liveChannelContainerRef}
-          onWheel={handleWheel}
-          className={cn("flex min-h-12 w-full gap-2 overflow-x-scroll", {})}
-        >
-          {nowLiveSorted.map((live) => {
-            return <LiveChannel key={live.id} video={live} />;
-          })}
-        </div>
-        <ToolButtonContainer />
-      </div>
-      <ToolButton
-        className={cn(
-          "right-2 top-0 z-20 rounded-none bg-base-2 p-1 transition-all md:px-5",
-          "absolute",
-          isBarActive ? "hidden" : "visible",
-        )}
-        icon={{
-          path: mdiChevronDown,
-          tooltip: "Open Panel",
-          onClick: openPanel,
-        }}
-      />
-    </>
+          </div>
+        </>
+      )}
+      <ToolButtonContainer icons={icons} />
+    </div>
   );
 }
--- packages/react/src/routes/multiview/multiview.tsx
+++ packages/react/src/routes/multiview/multiview.tsx
@@ -1,18 +1,109 @@
-import { Toolbar } from "@/components/multiview/Toolbar";
+import { ToolBar } from "@/components/multiview/toolbar/ToolBar";
+import {
+  MultiViewIcon,
+  ToolButton,
+} from "@/components/multiview/toolbar/ToolButton";
+import {
+  closeMultiViewPanelAtom,
+  isMobileAtom,
+  multiViewPanelOpenAtom,
+  openMultiViewPanelAtom,
+} from "@/hooks/useFrame";
+import { cn } from "@/lib/utils";
+import {
+  mdiCardPlusOutline,
+  mdiChevronDown,
+  mdiChevronUp,
+  mdiContentSaveOutline,
+  mdiDeleteOutline,
+  mdiFullscreen,
+  mdiGridLarge,
+  mdiLinkVariant,
+  mdiSync,
+  mdiTuneVertical,
+  mdiViewGridPlusOutline,
+} from "@mdi/js";
+import { useAtomValue, useSetAtom } from "jotai";
+import { useRef, useState } from "react";
 import { Helmet } from "@dr.pogodin/react-helmet";
+
+const reorderIcon =
+  "M2 2h8.8v8.8H2V2Zm11.3 11.3H22V22h-8.8v-8.8Zm4.6-10.9a.6.6 0 0 0-1 0l-3.9 4a.6.6 0 1 0 .9.9l3.5-3.6L21 7.3a.6.6 0 0 0 .8-1l-4-4Zm.1 10V2.8h-1.2v9.6H18ZM5.7 21.6c.3.3.7.3 1 0l3.9-4a.6.6 0 1 0-.9-.9l-3.5 3.6-3.6-3.6a.6.6 0 1 0-.9 1l4 4Zm-.2-10v9.6h1.3v-9.6H5.5Z";
 
 // multiview skeleton
 // selection bar at the top to change between orgs and allow url insertion
 // grid page for drag and drop
 
 export function Multiview() {
+  const isBarActive = useAtomValue(multiViewPanelOpenAtom);
+  const openPanel = useSetAtom(openMultiViewPanelAtom);
+  const multiviewRef = useRef<HTMLDivElement>(null);
+  const closePanel = useSetAtom(closeMultiViewPanelAtom);
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const isMobile = useAtomValue(isMobileAtom);
+  const toggleFullScreen = () => {
+    if (multiviewRef.current) {
+      if (isFullScreen) {
+        document.exitFullscreen();
+        setIsFullScreen(false);
+      } else {
+        multiviewRef.current.requestFullscreen();
+        setIsFullScreen(true);
+      }
+    }
+  };
+
+  const baseIcons: MultiViewIcon[] = [
+    { path: mdiCardPlusOutline, tooltip: "Open Dialog" },
+    { path: mdiGridLarge, tooltip: "Change Layout" },
+    { path: mdiViewGridPlusOutline, tooltip: "Add Cell" },
+    { path: mdiTuneVertical, tooltip: "Media Control" },
+    { path: reorderIcon, tooltip: "Reorder Layout" },
+  ];
+
+  const icons: MultiViewIcon[] = [
+    ...baseIcons,
+    { path: mdiSync, tooltip: "Archive Sync" },
+    { path: mdiContentSaveOutline, tooltip: "Save Layout" },
+    { path: mdiDeleteOutline, tooltip: "Clear" },
+    {
+      path: mdiFullscreen,
+      tooltip: isFullScreen ? "Exit Fullscreen" : "Enter Fullscreen",
+      onClick: toggleFullScreen,
+    },
+    { path: mdiLinkVariant, tooltip: "Share Layout" },
+    { path: mdiChevronUp, tooltip: "Collapse Panel", onClick: closePanel },
+  ];
+
+  const mobileIcons: MultiViewIcon[] = [
+    ...baseIcons,
+    { path: mdiLinkVariant, tooltip: "Share Layout" },
+    { path: mdiChevronUp, tooltip: "Collapse Panel", onClick: closePanel },
+  ];
+
   return (
     <>
       <Helmet>
         <title>Multiview - Holodex</title>
       </Helmet>
-      <div className="relative overflow-x-hidden">
-        <ToolBar />
+      <div id="multiview" ref={multiviewRef}>
+        <div>
+          <div className="flex relative h-full w-full flex-col">
+            <ToolBar icons={isMobile ? mobileIcons : icons} />
+            <ToolButton
+              className={cn(
+                "right-2 top-0 z-20 rounded-none bg-base-2 p-1 transition-all md:px-5",
+                "absolute",
+                isBarActive ? "hidden" : "visible",
+              )}
+              icon={{
+                path: mdiChevronDown,
+                tooltip: "Open Panel",
+                onClick: openPanel,
+              }}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
