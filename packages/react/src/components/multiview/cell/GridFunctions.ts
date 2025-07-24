2ee24dc3ce27a20f16c658392d65c3fe2308609e
Vincent Sit <vincentsit00@ymail.com>
2025-07-24 17:42:26 +1200

    Add horizontal decrease in size check to ensure minimum size is respected and adjust if the resize leads to breaking boundary

--- packages/react/src/components/multiview/cell/GridFunctions.ts
+++ packages/react/src/components/multiview/cell/GridFunctions.ts
@@ -50,11 +50,21 @@ export function onResizeStop(
   oldItem: Layout,
   newItem: Layout,
   minSize: number = 2,
+  limit: number = 24,
 ) {
+  // make sure that the newItem is at least minSize
   const directionImpacted = checkImpactDirection(newItem, oldItem);
+  if (newItem.w < minSize) {
+    newItem.w = minSize;
+    // if the resizing brings the item to beyond the edge of the grid, then we need to adjust the x position
+    if (newItem.x + newItem.w > limit) {
+      newItem.x = limit - newItem.w;
+    }
+  }
+  if (newItem.h < minSize) {
+    newItem.h = minSize;
+  }
   const itemsColliding = getCollidingItems(layout, newItem);
-  console.log(directionImpacted);
-  console.log(itemsColliding);
 
   for (const collidingItem of itemsColliding) {
     // iterate and check which direction the item is being impacted
